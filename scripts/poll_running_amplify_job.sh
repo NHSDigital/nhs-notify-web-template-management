ab15f30efedf34a2461080a31240cbe5c4add1d0
Chris Elliott <chris.elliott17@nhs.net>
2024-06-27 10:51:50 +0100

    Poll when missing job ID and add link from home page

--- scripts/poll_running_amplify_job.sh
+++ scripts/poll_running_amplify_job.sh
@@ -5,14 +5,27 @@ set -euo pipefail
 app_id=$1
 branch_name=$2
 
-sleep 15
-
-job_id=$( aws amplify list-jobs --app-id $app_id --branch-name $branch_name | jq -r '.jobSummaries[0].jobId' )
-
-wait_seconds=0
-wait_interval=10
+wait_interval=2
 max_wait_seconds=600
 
+# wait for Amplify job to be available
+wait_seconds=0
+while [ $wait_seconds -le $max_wait_seconds ]; do
+    job_id=$( aws amplify list-jobs --app-id $app_id --branch-name $branch_name | jq -r '.jobSummaries[0].jobId' || true )
+
+    if [[ $job_id != '' ]]; then
+        echo "Job ID found: |${job_id}|"
+        break
+    fi
+
+    echo "Job not found after ${wait_seconds} seconds. Waiting ${wait_interval} seconds and polling again"
+
+    sleep $wait_interval
+    wait_seconds=$(($wait_seconds + $wait_interval))
+done
+
+# wait for Amplify job to finish
+wait_seconds=0
 while [ $wait_seconds -le $max_wait_seconds ]; do
     job_status=$( aws amplify get-job --app-id $app_id --branch-name $branch_name --job-id $job_id | jq -r '.job.summary.status' )
 
