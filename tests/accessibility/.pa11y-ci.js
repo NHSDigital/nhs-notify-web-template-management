dcb6f24491e8d808d2824514c000b406216185f1
ben.hansell1 <ben.hansell1@nhs.net>
2024-07-10 15:49:16 +0100

    CCM-5671: update pa11y config to run faster and better

--- tests/accessibility/.pa11y-ci.js
+++ tests/accessibility/.pa11y-ci.js
@@ -3,25 +3,29 @@ const baseUrl = process.env.BASE_URL ?? 'localhost:3000';
 const { goToCreateNhsAppTemplatePage } = require('./actions/create-nhs-app-template.action');
 
 module.exports = {
-    defaults: {
-        reporters: [
-            'cli', // <-- this is the default reporter
-            [
-                'pa11y-ci-reporter-html',
-                {
-                    destination: './.reports/accessibility',
-                    includeZeroIssues: true
-                }
-            ],
-        ],
-            timeout: 20000,
-            wait: 2000,
-            chromeLaunchConfig: {
-            args: ['--no-sandbox']
-        },
-        useIncognitoBrowserContext: false,
-        standard: 'WCAG2AA', //'WCAG2AAA'
-        userAgent: 'pa11y-ci',
+  defaults: {
+    reporters: [
+      'cli', // <-- this is the default reporter
+      [
+        'pa11y-ci-reporter-html',
+        {
+          destination: './.reports/accessibility',
+          includeZeroIssues: true
+        }
+      ],
+    ],
+    timeout: 20000,
+    wait: 2000,
+    chromeLaunchConfig: {
+      args: ['--no-sandbox']
     },
-    urls: ['localhost:3000', 'localhost:3000/create-template', goToCreateNhsAppTemplatePage('localhost:3000/create-template')]
+    rules: [
+      'Principle1.Guideline1_3.1_3_1_AAA',
+    ],
+    useIncognitoBrowserContext: false,
+    standard: 'WCAG2AA', //'WCAG2AAA'
+    userAgent: 'pa11y-ci',
+    concurrency: 8,
+  },
+  urls: ['localhost:3000', 'localhost:3000/create-template', goToCreateNhsAppTemplatePage('localhost:3000/create-template')]
 };
