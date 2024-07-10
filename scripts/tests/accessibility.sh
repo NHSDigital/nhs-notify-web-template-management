3027af94201f5cfd9fed289b40fe5701f0d0e5f9
ben.hansell1 <ben.hansell1@nhs.net>
2024-07-10 15:35:26 +0100

    CCM-5671: build application before running tests

--- scripts/tests/accessibility.sh
+++ scripts/tests/accessibility.sh
@@ -4,13 +4,20 @@ set -euo pipefail
 
 is_local=$( [[ -z ${BASE_URL:-} || ${BASE_URL:-} == *"localhost"* ]] )
 if $is_local; then
+
+    # Build NextJS app
+    npm run build
+
+    # Run NextJS app in PM2 container
     npm run app:start
 
+    # Wait for the app to start
     npm run app:wait
 fi
 
 npm run test:accessibility
 
 if $is_local; then
+    # Stop PM2 container
     npm run app:stop
 fi
