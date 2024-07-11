9d01ac2d483c6e0841b9037f6dce562c56b3fa1f
ben.hansell1 <ben.hansell1@nhs.net>
2024-07-11 09:08:57 +0100

    CCM-5671: ensure env is ampylify app

--- terraform/components/amplify-branch/amplify_branch_branch.tf
+++ terraform/components/amplify-branch/amplify_branch_branch.tf
@@ -14,11 +14,11 @@ data "aws_ssm_parameter" "amplify_app_id" {
 }
 
 data "aws_ssm_parameter" "amplify_app_username" {
-  name = "/${var.project}/amplify-app/${var.environment}/amplify-repository-username"
+  name = "/${var.project}/amplify-app/${var.amplify_app_environment}/amplify-repository-username"
 }
 
 data "aws_ssm_parameter" "amplify_app_password" {
-  name = "/${var.project}/amplify-app/${var.environment}/amplify-repository-password"
+  name = "/${var.project}/amplify-app/${var.amplify_app_environment}/amplify-repository-password"
 }
 
 resource "aws_amplify_webhook" "webhook" {
