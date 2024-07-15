ce98500975a0de6302388dd08b70f88ffe6d2665
ben.hansell1 <ben.hansell1@nhs.net>
2024-07-15 13:48:30 +0100

    CCM-5671: dont export as function

--- tests/accessibility/.pa11y-ci.js
+++ tests/accessibility/.pa11y-ci.js
@@ -8,10 +8,10 @@ const {
   reviewNHSAppTemplateErrorPage,
 } = require('./actions');
 
-function pa11yConfig() {
-  const baseUrl = 'http://localhost:3000';
+const baseUrl = 'http://localhost:3000';
 
-  const urls = [
+module.exports = {
+  urls: [
     performCheck({ url: baseUrl, name: 'landing-page' }),
     performCheck(chooseATemplatePage(baseUrl)),
     performCheck(chooseATemplatePageError(baseUrl)),
@@ -19,32 +19,26 @@ function pa11yConfig() {
     performCheck(createNHSAppTemplateErrorPage(baseUrl)),
     performCheck(reviewNHSAppTemplatePage(baseUrl)),
     performCheck(reviewNHSAppTemplateErrorPage(baseUrl)),
-  ];
-
-  return {
-    urls,
-    defaults: {
-      reporters: [
-        'cli',
-        [
-          'pa11y-ci-reporter-html',
-          {
-            destination: './.reports/accessibility',
-            includeZeroIssues: true
-          }
-        ],
+  ],
+  defaults: {
+    reporters: [
+      'cli',
+      [
+        'pa11y-ci-reporter-html',
+        {
+          destination: './.reports/accessibility',
+          includeZeroIssues: true
+        }
       ],
-      rules: [
-        'Principle1.Guideline1_3.1_3_1_AAA',
-      ],
-      chromeLaunchOptions: {
-        args: ['--no-sandbox']
-      },
-      standard: 'WCAG2AA',
-      agent: 'pa11y',
-    }
-  };
-}
-
-module.exports = pa11yConfig();
+    ],
+    rules: [
+      'Principle1.Guideline1_3.1_3_1_AAA',
+    ],
+    chromeLaunchConfig: {
+      args: ['--no-sandbox']
+    },
+    standard: 'WCAG2AA',
+    agent: 'pa11y',
+  }
+};
 
