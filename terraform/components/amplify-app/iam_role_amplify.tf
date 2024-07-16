549cba9b80d2af8ca67a928d814ab82de077996f
Chris Elliott <chris.elliott17@nhs.net>
2024-07-16 08:48:44 +0100

    Fix policy attachment

--- terraform/components/amplify-app/iam_role_amplify.tf
+++ terraform/components/amplify-app/iam_role_amplify.tf
@@ -1,6 +1,8 @@
 resource "aws_iam_role" "amplify" {
   name               = "${local.csi}-amplify"
   assume_role_policy = data.aws_iam_policy_document.amplify_assume_role.json
+
+  description = "Amplify role for ${var.environment}"
 }
 
 data "aws_iam_policy_document" "amplify_assume_role" {
@@ -20,6 +22,6 @@ data "aws_iam_policy_document" "amplify_assume_role" {
   }
 }
 resource "aws_iam_role_policy_attachment" "amplify_standard_policy" {
-  role       = aws_iam_role.amplify.arn
+  role       = aws_iam_role.amplify.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmplifyBackendDeployFullAccess"
 }
