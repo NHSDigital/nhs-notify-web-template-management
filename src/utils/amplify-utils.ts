34adf52f0fa5abe4bceefbb6d734cf88b278e3a4
Chris Elliott <chris.elliott17@nhs.net>
2024-07-19 10:09:28 +0100

    Use fs

--- src/utils/amplify-utils.ts
+++ src/utils/amplify-utils.ts
@@ -1,10 +1,19 @@
 import { cookies } from 'next/headers';
+import fs from 'node:fs';
 import { generateServerClientUsingCookies } from '@aws-amplify/adapter-nextjs/data';
 import { Schema } from '../../amplify/data/resource';
-import config from '../../amplify_outputs.json';
+
+export const config = () => {
+  try {
+    const output = fs.readFileSync('../../amplify_outputs.json', 'utf8');
+    return JSON.parse(output);
+  } catch {
+    return {};
+  }
+};
 
 export const getAmplifyBackendClient = () =>
   generateServerClientUsingCookies<Schema>({
-    config,
+    config: config(),
     cookies,
   });
