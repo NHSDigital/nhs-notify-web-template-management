d0e6cff6f5f2df3ebfb3ba1316682b04bb7170a4
Chris Elliott <chris.elliott17@nhs.net>
2024-07-25 12:29:58 +0100

    Fixes after bad rebase

--- src/utils/amplify-utils.ts
+++ src/utils/amplify-utils.ts
@@ -4,8 +4,12 @@ import { generateServerClientUsingCookies } from '@aws-amplify/adapter-nextjs/da
 import { Schema } from '../../amplify/data/resource';
 
 export const config = () => {
-  const output = fs.readFileSync('amplify_outputs.json', 'utf8');
-  return JSON.parse(output);
+  try {
+    const output = fs.readFileSync('amplify_outputs.json', 'utf8');
+    return JSON.parse(output);
+  } catch {
+    return {};
+  }
 };
 
 export const getAmplifyBackendClient = () =>
