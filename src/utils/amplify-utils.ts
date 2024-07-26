c6f45584c45ad163be76c4ce2418d523305c8085
Chris Elliott <chris.elliott17@nhs.net>
2024-07-26 01:33:30 +0100

    Debugging

--- src/utils/amplify-utils.ts
+++ src/utils/amplify-utils.ts
@@ -1,19 +1,11 @@
 import { cookies } from 'next/headers';
-import { readFileSync } from 'node:fs';
 import { generateServerClientUsingCookies } from '@aws-amplify/adapter-nextjs/data';
 import { Schema } from '../../amplify/data/resource';
+import config from '../../amplify_outputs.json';
 
-export const config = () => {
-  try {
-    const output = readFileSync('amplify_outputs.json', 'utf8');
-    return JSON.parse(output);
-  } catch {
-    return {};
-  }
-};
 
 export const getAmplifyBackendClient = () =>
   generateServerClientUsingCookies<Schema>({
-    config: config(),
+    config: config,
     cookies,
   });
