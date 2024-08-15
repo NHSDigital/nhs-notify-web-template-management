c349c1f3539d7330a20037fe1f9b37a34753e4ae
Chris Elliott <chris.elliott17@nhs.net>
2024-08-15 15:30:50 +0100

    Fix linting

--- src/__tests__/components/atoms/NHSNotifySkipLink.test.tsx
+++ src/__tests__/components/atoms/NHSNotifySkipLink.test.tsx
@@ -3,64 +3,80 @@ import { NHSNotifySkipLink } from '@atoms/NHSNotifySkipLink/NHSNotifySkipLink';
 import userEvent from '@testing-library/user-event';
 
 test('puts focus on h1', async () => {
-    const user = userEvent.setup();
-    const container = render(<><NHSNotifySkipLink /><h1 id='heading'>heading</h1><p id='not-maincontent'>other</p></>);
+  const user = userEvent.setup();
+  const container = render(
+    <>
+      <NHSNotifySkipLink />
+      <h1 id='heading'>heading</h1>
+      <p id='not-maincontent'>other</p>
+    </>
+  );
 
-    expect(container.asFragment()).toMatchSnapshot();
+  expect(container.asFragment()).toMatchSnapshot();
 
-    const skipButton = document.getElementById('skip-link');
-    if (!skipButton) {
-        fail('Skip button not found');
-    }
+  const skipButton = document.querySelector('#skip-link');
+  if (!skipButton) {
+    throw new Error('Skip button not found');
+  }
 
-    await user.click(skipButton);
+  await user.click(skipButton);
 
-    const headingElement = document.getElementById('heading');
-    if (!headingElement) {
-        fail('heading element not found');
-    }
+  const headingElement = document.querySelector('#heading');
+  if (!headingElement) {
+    throw new Error('heading element not found');
+  }
 
-    expect(headingElement).toHaveFocus();
+  expect(headingElement).toHaveFocus();
 
-    const otherElement = document.getElementById('not-maincontent');
-    if (!otherElement) {
-        fail('not-maincontent element not found');
-    }
+  const otherElement = document.querySelector('#not-maincontent');
+  if (!otherElement) {
+    throw new Error('not-maincontent element not found');
+  }
 
-    await user.click(otherElement);
-    expect(headingElement).not.toHaveFocus();
+  await user.click(otherElement);
+  expect(headingElement).not.toHaveFocus();
 });
 
 test('puts focus on main content when there is no h1', async () => {
-    const user = userEvent.setup();
-    const container = render(<><NHSNotifySkipLink /><p id='maincontent'>heading</p></>);
+  const user = userEvent.setup();
+  const container = render(
+    <>
+      <NHSNotifySkipLink />
+      <p id='maincontent'>heading</p>
+    </>
+  );
 
-    expect(container.asFragment()).toMatchSnapshot();
+  expect(container.asFragment()).toMatchSnapshot();
 
-    const skipButton = document.getElementById('skip-link');
+  const skipButton = document.querySelector('#skip-link');
 
-    if (!skipButton) {
-        fail('Skip button not found');
-    }
+  if (!skipButton) {
+    throw new Error('Skip button not found');
+  }
 
-    await user.click(skipButton);
+  await user.click(skipButton);
 
-    expect(document.getElementById('maincontent')).toHaveFocus();
+  expect(document.querySelector('#maincontent')).toHaveFocus();
 });
 
 test('does nothing when there is no h1 or #maincontent', async () => {
-    const user = userEvent.setup();
-    const container = render(<><NHSNotifySkipLink /><p id='not-maincontent'>heading</p></>);
+  const user = userEvent.setup();
+  const container = render(
+    <>
+      <NHSNotifySkipLink />
+      <p id='not-maincontent'>heading</p>
+    </>
+  );
 
-    expect(container.asFragment()).toMatchSnapshot();
+  expect(container.asFragment()).toMatchSnapshot();
 
-    const skipButton = document.getElementById('skip-link');
+  const skipButton = document.querySelector('#skip-link');
 
-    if (!skipButton) {
-        fail('Skip button not found');
-    }
+  if (!skipButton) {
+    throw new Error('Skip button not found');
+  }
 
-    await user.click(skipButton);
+  await user.click(skipButton);
 
-    expect(skipButton).toHaveFocus();
+  expect(skipButton).toHaveFocus();
 });
