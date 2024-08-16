a96fabbe6532dd0f746ee12afdae1aa314b231fa
Mark Ramsden <mark.ramsden5@nhs.net>
2024-08-16 14:14:49 +0100

    CCM-6212: minor layout change

--- src/app/invalid-session/page.tsx
+++ src/app/invalid-session/page.tsx
@@ -8,20 +8,18 @@ const { invalidSessionPageContent } = content.pages;
 export default function InvalidSessionPage() {
   return (
     <div className='nhsuk-grid-row' data-testid='page-content-wrapper'>
-      <div className='nhsuk-grid-column-two-thirds'>
-        <h1 className='nhsuk-heading-xl' data-testid='page-heading'>
-          {invalidSessionPageContent.pageHeading}
-        </h1>
+      <h1 className='nhsuk-heading-xl' data-testid='page-heading'>
+        {invalidSessionPageContent.pageHeading}
+      </h1>
 
-        <p>{invalidSessionPageContent.description}</p>
+      <p>{invalidSessionPageContent.description}</p>
 
-        <BackLink
-          href={invalidSessionPageContent.backLink.path}
-          className='nhsuk-u-margin-bottom-7 nhsuk-u-margin-left-3'
-        >
-          {invalidSessionPageContent.backLink.text}
-        </BackLink>
-      </div>
+      <BackLink
+        href={invalidSessionPageContent.backLink.path}
+        className='nhsuk-u-margin-bottom-7 nhsuk-u-margin-left-3'
+      >
+        {invalidSessionPageContent.backLink.text}
+      </BackLink>
     </div>
   );
 }
