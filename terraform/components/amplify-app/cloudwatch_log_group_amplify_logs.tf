137243be57c9641fa67df51ab3cf40e92282b899
Chris Elliott <chris.elliott17@nhs.net>
2024-08-19 13:22:09 +0100

    Fix terraform references

--- terraform/components/amplify-app/cloudwatch_log_group_amplify_logs.tf
+++ terraform/components/amplify-app/cloudwatch_log_group_amplify_logs.tf
@@ -4,13 +4,13 @@ resource "aws_cloudwatch_log_group" "amplify_logs" {
 }
 
 resource "aws_iam_policy" "amplify_cloudwatch_access" {
-  name        = "${local.csi}-${var.lambda_options.function_name}"
-  description = "Permissions provided to event-driven lambda function ${var.lambda_options.function_name}"
+  name        = "${local.csi}-amplify-cloudwatch-access"
+  description = "Permissions required to create logs in the ${aws_cloudwatch_log_group.amplify_logs.name} log group"
   path        = "/"
-  policy      = data.aws_iam_policy_document.lambda_function.json
+  policy      = data.aws_iam_policy_document.amplify_cloudwatch_access.json
 }
 
-data "aws_iam_policy_document" "lambda_function" {
+data "aws_iam_policy_document" "amplify_cloudwatch_access" {
   statement {
     sid    = "AllowLogs"
     effect = "Allow"
