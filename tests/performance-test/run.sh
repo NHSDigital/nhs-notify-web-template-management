ee4fbff98668b0f875526a65ab317c53250daaea
priyanka.gujar2 <priyanka.gujar2@nhs.net>
2024-08-20 10:10:37 +0100

    CCM-6246: removed credentials
    Signed-off-by: priyanka.gujar2 <priyanka.gujar2@nhs.net>

--- tests/performance-test/run.sh
+++ tests/performance-test/run.sh
@@ -3,4 +3,4 @@
 TIMESTAMP=$(date +%Y_%m_%d_%H_%M_%S)
 
 cd results
-exec poetry run locust --users 1 --run-time 30s -H 'https://main.d1ie8muegpw33w.amplifyapp.com' -f ../shared.py,../create_nhsapp_template.py --class-picker $*  --user-name='send' --password='messages' --html="report_$TIMESTAMP.html" --log-transactions-in-file --logfile="log_$TIMESTAMP.txt" 
+exec poetry run locust --users 1 --run-time 30s -H 'https://main.d1ie8muegpw33w.amplifyapp.com' -f ../shared.py,../create_nhsapp_template.py --class-picker $*  --user-name=[username] --password=[password] --html="report_$TIMESTAMP.html" --log-transactions-in-file --logfile="log_$TIMESTAMP.txt" 
