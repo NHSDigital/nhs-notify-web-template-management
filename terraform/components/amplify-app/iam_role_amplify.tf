8867345202d9b0e4e2b953ef53e07829349959fa
muhammed.salaudeen <muhammed.salaudeen1@nhs.net>
2024-08-21 11:28:30 +0100

    CCM-5521: Setup terraform for amplify SES policy

--- terraform/components/amplify-app/iam_role_amplify.tf
+++ terraform/components/amplify-app/iam_role_amplify.tf
@@ -27,16 +27,22 @@ resource "aws_iam_role_policy_attachment" "amplify_standard_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "amplify_ses_policy" {
+  count = var.deploy_ses_email_identity ? 1 : 0
+
   role       = aws_iam_role.amplify.name
-  policy_arn = aws_iam_policy.amplify_ses.arn
+  policy_arn = aws_iam_policy.amplify_ses[0].arn
 }
 
 resource "aws_iam_policy" "amplify_ses" {
+  count = var.deploy_ses_email_identity ? 1 : 0
+
   name   = "${local.csi}-amplify-ses"
-  policy = data.aws_iam_policy_document.amplify_ses.json
+  policy = data.aws_iam_policy_document.amplify_ses[0].json
 }
 
 data "aws_iam_policy_document" "amplify_ses" {
+  count = var.deploy_ses_email_identity ? 1 : 0
+
   statement {
     effect  = "Allow"
     actions = [
--- terraform/components/amplify-app/ses_email_identity_test_mailbox.tf
+++ terraform/components/amplify-app/ses_email_identity_test_mailbox.tf
@@ -1,4 +1,5 @@
 resource "aws_ses_email_identity" "test_mailbox" {
-  count = var.deploy_ses_email_identity == "true" ? 1 : 0
+  count = var.deploy_ses_email_identity ? 1 : 0
+
   email = "england.test.cm@nhs.net"
 }
