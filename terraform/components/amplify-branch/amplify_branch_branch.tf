4cb8b0cac36027578dccf3a1a524c2a235e91475
sidnhs <siddharth.nair1@nhs.net>
2024-08-23 13:47:16 +0100

    CCM-6250: Update modules to point to public github repo

--- terraform/components/amplify-branch/amplify_branch_branch.tf
+++ terraform/components/amplify-branch/amplify_branch_branch.tf
@@ -1,12 +1,23 @@
-resource "aws_amplify_branch" "branch" {
-  app_id      = data.aws_ssm_parameter.amplify_app_id.value
-  branch_name = var.branch_name
+module "amp_branch" {
+  source = "git::https://github.com/NHSDigital/nhs-notify-shared-modules.git//infrastructure/modules/amp_branch?ref=v1.0.0"
+  
+  name           = var.branch_name
+  aws_account_id = var.account_ids["notify-web-template-management"]
+  component      = var.component
+  environment    = var.environment
+  project        = var.project
+  region         = var.region
+  group          = var.group
+  
+  amplify_app_id      = data.aws_ssm_parameter.amplify_app_id.value
+  branch = var.branch_name
   framework   = "Next.js - SSR"
   stage       = local.is_production ? "PRODUCTION" : "DEVELOPMENT"
 
   description = "Amplify branch for ${var.branch_name}"
 
   enable_auto_build = false
+  enable_pull_request_preview = false
 
   environment_variables = {
     BACKEND_BRANCH = var.backend_branch
@@ -19,6 +30,6 @@ data "aws_ssm_parameter" "amplify_app_id" {
 
 resource "aws_amplify_webhook" "webhook" {
   app_id      = data.aws_ssm_parameter.amplify_app_id.value
-  branch_name = aws_amplify_branch.branch.branch_name
+  branch_name = module.amp_branch.name
   description = "${var.branch_name} webhook"
 }
