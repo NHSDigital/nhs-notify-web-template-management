d412bef10f0b591b322fba2a217aced175ef9995
sidnhs <siddharth.nair1@nhs.net>
2024-08-23 14:16:16 +0100

    CCM-6250: Update modules to point to public github repo

--- terraform/components/amplify-branch/amplify_branch_branch.tf
+++ terraform/components/amplify-branch/amplify_branch_branch.tf
@@ -1,24 +1,19 @@
 module "amp_branch" {
-  source = "git::https://github.com/NHSDigital/nhs-notify-shared-modules.git//infrastructure/modules/amp_branch?ref=v1.0.0"
-  
-  name           = var.branch_name
-  aws_account_id = var.account_ids["notify-web-template-management"]
-  component      = var.component
-  environment    = var.environment
-  project        = var.project
-  region         = var.region
-  group          = var.group
-  
-  amplify_app_id      = data.aws_ssm_parameter.amplify_app_id.value
-  branch = var.branch_name
-  framework   = "Next.js - SSR"
-  stage       = local.is_production ? "PRODUCTION" : "DEVELOPMENT"
-
-  description = "Amplify branch for ${var.branch_name}"
-
-  enable_auto_build = false
+  source                      = "git::https://github.com/NHSDigital/nhs-notify-shared-modules.git//infrastructure/modules/amp_branch?ref=v1.0.0"
+  name                        = var.branch_name
+  aws_account_id              = var.account_ids["notify-web-template-management"]
+  component                   = var.component
+  environment                 = var.environment
+  project                     = var.project
+  region                      = var.region
+  group                       = var.group
+  amplify_app_id              = data.aws_ssm_parameter.amplify_app_id.value
+  branch                      = var.branch_name
+  framework                   = "Next.js - SSR"
+  stage                       = local.is_production ? "PRODUCTION" : "DEVELOPMENT"
+  description                 = "Amplify branch for ${var.branch_name}"
+  enable_auto_build           = false
   enable_pull_request_preview = false
-
   environment_variables = {
     BACKEND_BRANCH = var.backend_branch
   }
