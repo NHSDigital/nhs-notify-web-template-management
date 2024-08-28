662edeee9560aea71296f0f48065acefcf708b21
sidnhs <siddharth.nair1@nhs.net>
2024-08-28 09:41:44 +0100

    CCM-6250: fmt

--- terraform/components/amplify-branch/module_amp_branch.tf
+++ terraform/components/amplify-branch/module_amp_branch.tf
@@ -1,5 +1,6 @@
 module "amp_branch" {
-  source                      = "git::https://github.com/NHSDigital/nhs-notify-shared-modules.git//infrastructure/modules/amp_branch?ref=v1.0.0"
+  source = "git::https://github.com/NHSDigital/nhs-notify-shared-modules.git//infrastructure/modules/amp_branch?ref=v1.0.0"
+
   name                        = var.branch_name
   aws_account_id              = var.account_ids["notify-web-template-management"]
   component                   = var.component
