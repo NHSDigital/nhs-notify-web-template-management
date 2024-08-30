414aa1dacd4fb865312f87bdbc00e36fc2b8e0a1
aidenvaines-bjss <aiden.vaines@bjss.com>
2024-08-30 10:22:16 +0100

    CCM-6104 linting

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? '/templates';
 
-
 const nextConfig = {
   basePath,
-
   env: {
     basePath,
   },
