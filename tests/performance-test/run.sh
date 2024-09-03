31b1d7e075cf81e5f886d129099cef722f57fdd7
priyanka.gujar2 <priyanka.gujar2@nhs.net>
2024-09-03 16:11:39 +0100

    CCM-6246 : first perf test script

---
+++ tests/performance-test/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+TIMESTAMP=$(date +%Y_%m_%d_%H_%M_%S)
+
+cd results
+exec poetry run locust --users 1 --run-time 30s -H 'https://main.d1ie8muegpw33w.amplifyapp.com' -f ../shared.py,../create_nhsapp_template.py --class-picker $*  --user-name=[username] --password=[password] --html="report_$TIMESTAMP.html" --log-transactions-in-file --logfile="log_$TIMESTAMP.txt"
