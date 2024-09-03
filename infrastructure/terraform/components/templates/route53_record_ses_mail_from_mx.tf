c3b9f7d5975d883f05ae60a1488c45019f2ce43e
Chris Elliott <chris.elliott17@nhs.net>
2024-09-03 13:28:19 +0100

    Code cleanup

--- infrastructure/terraform/components/templates/route53_record_ses_mail_from_mx.tf
+++ infrastructure/terraform/components/templates/route53_record_ses_mail_from_mx.tf
@@ -3,5 +3,5 @@ resource "aws_route53_record" "ses_mail_from_mx" {
   name    = aws_ses_domain_mail_from.main.mail_from_domain
   type    = "MX"
   ttl     = "600"
-  records = ["10 feedback-smtp.eu-west-2.amazonses.com"] # Change to the region in which `aws_ses_domain_identity.example` is created
+  records = ["10 feedback-smtp.eu-west-2.amazonses.com"]
 }
--- infrastructure/terraform/components/templates/ses_domain_mail_from.tf
+++ infrastructure/terraform/components/templates/ses_domain_mail_from.tf
@@ -1,4 +1,3 @@
-
 resource "aws_ses_domain_mail_from" "main" {
   domain           = aws_ses_domain_identity.main.domain
   mail_from_domain = "mail.${aws_ses_domain_identity.main.domain}"
