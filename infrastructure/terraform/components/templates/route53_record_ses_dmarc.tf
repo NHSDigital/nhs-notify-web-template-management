4cea40018d723c66ef37d9e03acead8e8e1cd22c
Chris Elliott <chris.elliott17@nhs.net>
2024-09-04 11:04:33 +0100

    SES debugging

--- infrastructure/terraform/components/templates/route53_record_ses_dmarc.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_route53_record" "ses_dmarc" {
-  zone_id = local.acct.dns_zone["id"]
-  name    = "_dmarc.${aws_ses_domain_identity.main.id}"
-  type    = "TXT"
-  ttl     = "300"
-  records = [
-    "v=DMARC1; p=none;"
-  ]
-}
--- infrastructure/terraform/components/templates/ses_domain_dkim.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_ses_domain_dkim" "main" {
-  domain = aws_ses_domain_identity.main.domain
-}
