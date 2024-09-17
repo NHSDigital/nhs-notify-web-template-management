cdba9e3173c0e96b0d93473ae32b94c25f60973e
Chris Elliott <chris.elliott17@nhs.net>
2024-09-17 10:31:45 +0100

    Allow requests from web-gateway origin

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? '/templates';
+const domain = process.env.NOTIFY_DOMAIN_NAME ?? 'main.templates.nonprod.nhsnotify.national.nhs.uk';
 
 const nextConfig = {
   basePath,
   env: {
     basePath,
+  },
+
+  experimental: {
+    serverActions: {
+      allowedOrigins: [domain, domain.replace('templates', 'web-gateway')],
+    },
   },
 
   async redirects() {
