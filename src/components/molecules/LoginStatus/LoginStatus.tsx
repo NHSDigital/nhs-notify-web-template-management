70b3fa3908c0149add57711aafd40c34dc3f1316
Chris Elliott <chris.elliott17@nhs.net>
2024-09-17 13:50:22 +0100

    Auth testing

---
+++ src/components/molecules/LoginStatus/LoginStatus.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { Header } from 'nhsuk-react-components';
+import React, { useEffect, useState } from 'react';
+import { useAuthenticator } from '@aws-amplify/ui-react';
+import { fetchAuthSession, signOut } from 'aws-amplify/auth';
+import { JwtPayload } from 'aws-jwt-verify/jwt-model';
+
+export default function LoginStatus() {
+  const { authStatus } = useAuthenticator();
+  const [idToken, setIdToken] = useState<JwtPayload | undefined>();
+  useEffect(() => {
+    (async () => {
+      const session = await fetchAuthSession();
+      setIdToken(session.tokens?.idToken?.payload);
+    })().catch(console.error);
+  }, [authStatus]);
+
+  switch (authStatus) {
+    case 'authenticated': {
+      return [
+        <Header.ServiceName key='serviceName'>
+          {idToken?.email?.toString() || ''}
+        </Header.ServiceName>,
+        <Header.NavItem key='navItem' onClick={() => signOut()}>
+          Sign out
+        </Header.NavItem>,
+      ];
+    }
+    case 'unauthenticated': {
+      return (
+        <Header.NavItem href={`https://chel5-auth-poc-rebased.d11o7gqmt8o0cx.amplifyapp.com/auth?redirect=${encodeURIComponent(location.href)}`}>
+          Sign in
+        </Header.NavItem>
+      );
+    }
+    default: {
+      return null;
+    }
+  }
+}
