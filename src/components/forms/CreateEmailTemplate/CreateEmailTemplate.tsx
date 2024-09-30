9ca6bcfb6cfbd2e3fe102abd89b818d37c012c7c
Fawad Ali <fawad.ali3@nhs.net>
2024-09-30 12:47:41 +0100

    CCM-4893: fix build issues again

--- src/components/forms/CreateEmailTemplate/CreateEmailTemplate.tsx
+++ src/components/forms/CreateEmailTemplate/CreateEmailTemplate.tsx
@@ -75,9 +75,7 @@ export const CreateEmailTemplate: FC<PageComponentProps> = ({
 
   return (
     <div className='nhsuk-grid-row'>
-      <NHSNotifyBackButton
-        formId='create-email-template-back' action={action}
-      >
+      <NHSNotifyBackButton formId='create-email-template-back' action={action}>
         <input type='hidden' name='emailTemplateName' value={templateName} />
         <input
           type='hidden'
