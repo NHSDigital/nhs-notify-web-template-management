3c0cf36576f38f9104ba72b35e952712ae5fde5f
Chris Elliott <chris.elliott17@nhs.net>
2024-09-30 14:34:11 +0100

    Add Amplify backend output

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -26,3 +26,45 @@ backend.data.resources.cfnResources.amplifyDynamoDbTables.SessionStorage.timeToL
     attributeName: 'ttl',
     enabled: true,
   };
+
+const userPoolId = process.env.USER_POOL_ID;
+const userPoolClientId = process.env.USER_POOL_CLIENT_ID;
+const hostedLoginDomain = `auth.${process.env.NOTIFY_DOMAIN_NAME}`;
+const domainName = `${process.env.NOTIFY_ENVIRONMENT}.${process.env.NOTIFY_DOMAIN_NAME}`;
+
+backend.addOutput({
+  auth: {
+    aws_region: 'eu-west-2',
+    user_pool_id: userPoolId,
+    user_pool_client_id: userPoolClientId,
+    oauth: {
+      identity_providers: [],
+      domain: hostedLoginDomain,
+      scopes: [
+        'openid',
+        'email',
+        'profile',
+        'phone',
+        'aws.cognito.signin.user.admin',
+      ],
+      redirect_sign_in_uri: [
+        `https://${domainName}/auth/`,
+      ],
+      redirect_sign_out_uri: [
+        `https://${domainName}/`,
+      ],
+      response_type: 'code',
+    },
+    username_attributes: ['email'],
+    standard_required_attributes: ['email'],
+    user_verification_types: ['email'],
+    unauthenticated_identities_enabled: false,
+    password_policy: {
+      min_length: 8,
+      require_lowercase: true,
+      require_uppercase: true,
+      require_numbers: true,
+      require_symbols: true,
+    },
+  },
+});
