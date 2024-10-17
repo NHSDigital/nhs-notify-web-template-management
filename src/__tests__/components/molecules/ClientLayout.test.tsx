9b70566406d1c31c49f39c2ace810d54ecf50972
Chris Elliott <chris.elliott17@nhs.net>
2024-10-17 15:10:45 +0100

    Fix ClientLayout snapshot

--- src/__tests__/components/molecules/ClientLayout.test.tsx
+++ src/__tests__/components/molecules/ClientLayout.test.tsx
@@ -1,5 +1,13 @@
+import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react';
 import { render, waitFor } from '@testing-library/react';
 import { ClientLayout } from '@molecules/ClientLayout/ClientLayout';
+
+jest.mock('@aws-amplify/ui-react', () => ({
+    ...jest.requireActual('@aws-amplify/ui-react'),
+    useAuthenticator: () => ({
+        authStatus: 'authenticated'
+    }),
+}))
 
 test('ClientLayout', async () => {
   const container = render(
