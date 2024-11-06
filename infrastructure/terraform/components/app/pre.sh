3dd30f7f04f759af5d45e8a23bb2adc2dfd3a7a3
Michael Harrison <michael.harrison31@nhs.net>
2024-11-06 15:10:13 +0000

    CCM-7249: copy final echo from other repo

--- infrastructure/terraform/components/app/pre.sh
+++ infrastructure/terraform/components/app/pre.sh
@@ -9,12 +9,9 @@ set -x
 original_dir=$( pwd )
 
 cd "$( git rev-parse --show-toplevel )/infrastructure/api"
+
 npm ci
 npm run build
 echo "build succeeded"
 
-
-cd $original_dir
-echo "pre script finished $( pwd )"
-
-# exit 0
+echo -e "All pre actions complete!\n"
