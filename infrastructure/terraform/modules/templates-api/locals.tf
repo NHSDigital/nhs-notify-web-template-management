3bcfbb709bdf10c0cc5073e7106ebb5328d21be8
Michael Harrison <michael.harrison31@nhs.net>
2024-11-08 14:35:47 +0000

    CCM-7249: fix path

--- infrastructure/terraform/modules/templates-api/locals.tf
+++ infrastructure/terraform/modules/templates-api/locals.tf
@@ -1,7 +1,7 @@
 locals {
   csi = "${var.csi}-${var.component}"
 
-  api_source_code_directory = "${path.module}/../../../../api"
+  api_source_code_directory = abspath("${path.module}/../../../../api")
 
   openapi_spec = templatefile("${local.api_source_code_directory}/spec.json", {
     APIG_EXECUTION_ROLE_ARN = aws_iam_role.api_gateway_execution_role.arn
