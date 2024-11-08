810996ac17e2e64c10f3219232e70219e9e3cda6
Michael Harrison <michael.harrison31@nhs.net>
2024-11-08 10:28:23 +0000

    CCM-7249: commit the right things

--- infrastructure/terraform/modules/templates-api/cloudwatch_log_group_authorizer_lambda.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_cloudwatch_log_group" "authorizer_lambda" {
-  name              = "/aws/lambda/${local.authorizer_lambda_function_name}"
-  retention_in_days = var.log_retention_in_days
-}
