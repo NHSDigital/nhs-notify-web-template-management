b6507d247dbf0f368378b5ff4e2225f114169b27
Michael Harrison <michael.harrison31@nhs.net>
2024-11-08 18:21:11 +0000

    CCM-7249: fix path

--- infrastructure/terraform/modules/typescript-build-zip/locals.tf
+++ infrastructure/terraform/modules/typescript-build-zip/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  entrypoint_stem = trimsuffix(var.entrypoint, ".ts")
+  entrypoint_stem = trimsuffix(basename(var.entrypoint), ".ts")
 
   output_path = "${var.source_code_dir}/.build/${local.entrypoint_stem}.zip"
 }
