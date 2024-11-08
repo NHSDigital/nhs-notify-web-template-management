5075d96404e1bc98c2e45893edea75956b3c7c11
Michael Harrison <michael.harrison31@nhs.net>
2024-11-08 14:52:40 +0000

    CCM-7249: testing

--- infrastructure/terraform/modules/templates-api/locals.tf
+++ infrastructure/terraform/modules/templates-api/locals.tf
@@ -1,9 +1,10 @@
 locals {
   csi = "${var.csi}-${var.component}"
 
-  api_source_code_directory = abspath("${path.module}/../../../../api")
+  api_source_code_directory = abspath("${path.module}")
 
   openapi_spec = templatefile("${local.api_source_code_directory}/spec.json", {
+    AWS_REGION              = var.region
     APIG_EXECUTION_ROLE_ARN = aws_iam_role.api_gateway_execution_role.arn
     AUTHORIZER_LAMBDA_ARN   = module.authorizer_lambda.function_arn
     ENDPOINT_LAMBDA_ARN     = module.endpoint_lambda.function_arn
