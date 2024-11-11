448a41d74c69bb4fe516ea99aa8b3c09f7ef3895
Michael Harrison <michael.harrison31@nhs.net>
2024-11-11 10:17:18 +0000

    CCM-7249: always run ts build

--- infrastructure/terraform/modules/typescript-build-zip/null_resource_typescript_build.tf
+++ infrastructure/terraform/modules/typescript-build-zip/null_resource_typescript_build.tf
@@ -1,4 +1,8 @@
 resource "null_resource" "typescript_build" {
+  triggers = {
+    always_run = timestamp()
+  }
+
   provisioner "local-exec" {
     working_dir = var.source_code_dir
     command     = "npm ci && npm run build"
