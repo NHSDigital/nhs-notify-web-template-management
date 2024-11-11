e83916fd9caad3fb214b26d91193486f7be0cb0b
Michael Harrison <michael.harrison31@nhs.net>
2024-11-11 12:40:27 +0000

    CCM-7249: fix templatefile syntax

--- infrastructure/terraform/modules/templates-api/locals.tf
+++ infrastructure/terraform/modules/templates-api/locals.tf
@@ -3,7 +3,7 @@ locals {
 
   lambdas_source_code_dir = abspath("${path.module}/../../../../lambdas")
 
-  openapi_spec = templatefile("${path.module}/spec.json", {
+  openapi_spec = templatefile("${path.module}/spec.tmpl.json", {
     AWS_REGION              = var.region
     APIG_EXECUTION_ROLE_ARN = aws_iam_role.api_gateway_execution_role.arn
     AUTHORIZER_LAMBDA_ARN   = module.authorizer_lambda.function_arn
