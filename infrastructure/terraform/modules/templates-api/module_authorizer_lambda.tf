cb6580cf8aeeccd38ff3d7ed0c46cc7fa79b5cd5
Michael Harrison <michael.harrison31@nhs.net>
2024-11-14 17:57:44 +0000

    CCM-7249: typescript config for lambdas

--- infrastructure/terraform/modules/templates-api/module_authorizer_lambda.tf
+++ infrastructure/terraform/modules/templates-api/module_authorizer_lambda.tf
@@ -5,6 +5,7 @@ module "authorizer_lambda" {
   function_name    = "${local.csi}-authorizer"
   filename         = module.authorizer_build.output_path
   source_code_hash = module.authorizer_build.base64sha256
+  runtime          = "nodejs20.x"
   handler          = "index.handler"
 
   log_retention_in_days = var.log_retention_in_days
--- infrastructure/terraform/modules/templates-api/module_endpoint_lambda.tf
+++ infrastructure/terraform/modules/templates-api/module_endpoint_lambda.tf
@@ -5,6 +5,7 @@ module "endpoint_lambda" {
   function_name    = "${local.csi}-endpoint"
   filename         = module.endpoint_build.output_path
   source_code_hash = module.endpoint_build.base64sha256
+  runtime          = "nodejs20.x"
   handler          = "index.handler"
 
   log_retention_in_days = var.log_retention_in_days
