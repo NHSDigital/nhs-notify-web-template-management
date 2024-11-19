3433bd6771698b55256ed96820542a9e2cd52288
Chris Elliott <chris.elliott17@nhs.net>
2024-11-19 14:54:40 +0000

    Fix terraform validation errors

--- infrastructure/terraform/components/app/ssm_parameter_cognito_config.tf
+++ infrastructure/terraform/components/app/ssm_parameter_cognito_config.tf
@@ -3,7 +3,10 @@ resource "aws_ssm_parameter" "cognito_config" {
   name        = "/${local.csi}/cognito_config"
   description = "Configuration values for Cognito instance"
   type        = "SecureString"
-  value       = data.aws_ssm_parameter.sftp_mock_config.value
+  value       = jsonencode({
+    user_pool_id = "placeholder"
+    user_pool_client_id = "placeholder"
+  })
 
   lifecycle {
     ignore_changes = [
