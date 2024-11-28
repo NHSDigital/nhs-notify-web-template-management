ec4ba30debae17e1fd807cfc9da2e285ff1c2ace
Michael Harrison <michael.harrison31@nhs.net>
2024-11-28 12:35:01 +0000

    CCM-7409: auth util script

--- infrastructure/terraform/components/sandbox/outputs.tf
+++ infrastructure/terraform/components/sandbox/outputs.tf
@@ -1,3 +1,11 @@
 output "api_base_url" {
   value = module.templates_api.api_base_url
 }
+
+output "cognito_user_pool_id" {
+  value = aws_cognito_user_pool.sandbox.id
+}
+
+output "cognito_user_pool_client_id" {
+  value = aws_cognito_user_pool_client.sandbox.id
+}
