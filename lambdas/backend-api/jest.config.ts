5974a7286ba4122475135ab1d039e162895193a3
ben.hansell1 <ben.hansell1@nhs.net>
2024-11-29 16:20:21 +0000

    CCM-7525: add api handlers to index file todo a single build while we wait for CCM-7409

--- lambdas/backend-api/jest.config.ts
+++ lambdas/backend-api/jest.config.ts
@@ -1,12 +1,19 @@
 module.exports = {
   transform: { '\\.ts$': '@swc/jest' },
-  testPathIgnorePatterns: ['logger.ts', 'schema-for.ts', 'src/app/index.ts'],
+  testPathIgnorePatterns: ['logger.ts', 'schema-for.ts', './src/index.ts'],
   coverageProvider: 'babel',
   coverageThreshold: {
     global: {
       branches: 100,
       functions: 100,
       lines: 100,
+      statements: -10,
+    },
+    // Remove after CCM-7409
+    'src/index.ts': {
+      branches: 70,
+      functions: 100,
+      lines: 90,
       statements: -10,
     },
   },
