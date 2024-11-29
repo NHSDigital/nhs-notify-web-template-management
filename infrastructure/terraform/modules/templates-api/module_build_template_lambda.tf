c1a9b9ff73e6866db530ad10bab524acc62c043e
ben.hansell1 <ben.hansell1@nhs.net>
2024-11-29 16:26:27 +0000

    CCM-7525: fix tf path

--- infrastructure/terraform/modules/templates-api/module_build_template_lambda.tf
+++ infrastructure/terraform/modules/templates-api/module_build_template_lambda.tf
@@ -1,6 +1,6 @@
 module "build_template_lambda" {
   source = "../typescript-build-zip"
 
-  source_code_dir = "${local.lambdas_source_code_dir}/endpoint"
+  source_code_dir = "${local.lambdas_source_code_dir}/backend-api"
   entrypoint      = "src/index.ts"
 }
