f7b63f3a0089b4cad9ea5043ca70a563973f0357
Ian Hodges <ian.hodges1@nhs.net>
2024-12-04 11:14:24 +0000

    CCM-7585: move createTemplateButton to manage templates file

--- tests/test-team/pages/template-mgmt-manage-templates-page.ts
+++ tests/test-team/pages/template-mgmt-manage-templates-page.ts
@@ -1,10 +1,25 @@
+import { Locator, Page } from '@playwright/test';
 import { TemplateMgmtBasePage } from './template-mgmt-base-page';
 
 export class ManageTemplatesPage extends TemplateMgmtBasePage {
   private static manageTemplatesPageUrl = '/templates/manage-templates';
 
-  // No sessionId needed now, so just navigate to the base URL
+  readonly createTemplateButton: Locator;
+
+  constructor(page: Page) {
+    super(page);
+
+    this.createTemplateButton = page
+      .locator('[class="nhsuk-button"]')
+      .and(page.getByRole('button'))
+      .and(page.getByText('Create template'));
+  }
+
   async loadPage(): Promise<void> {
     await this.navigateTo(ManageTemplatesPage.manageTemplatesPageUrl);
+  }
+
+  async clickCreateTemplateButton() {
+    await this.createTemplateButton.click();
   }
 }
