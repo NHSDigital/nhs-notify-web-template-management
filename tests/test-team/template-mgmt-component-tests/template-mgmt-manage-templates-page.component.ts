1d37e044343e2411eae477c515e6187b351e4423
Ian Hodges <ian.hodges1@nhs.net>
2024-12-04 10:57:10 +0000

    CCM-7585: add not yet submitted status tests

--- tests/test-team/template-mgmt-component-tests/template-mgmt-manage-templates-page.component.ts
+++ tests/test-team/template-mgmt-component-tests/template-mgmt-manage-templates-page.component.ts
@@ -10,34 +10,58 @@ import {
 import { TemplateFactory } from '../helpers/template-factory';
 import { TemplateStatus, TemplateType } from '../helpers/types';
 import { TemplateStorageHelper } from '../helpers/template-storage-helper';
+import { v4 as uuid } from 'uuid';
 
 const templates = {
-  email: TemplateFactory.create({
-    id: 'valid-email-template',
+  emailSubmitted: TemplateFactory.create({
+    id: uuid(),
     version: 1,
-    name: 'test-template-cat-email',
+    name: 'email-submitted_manage-templates-page',
     message: 'test example message',
     subject: 'test example subject',
     templateType: TemplateType.EMAIL,
     templateStatus: TemplateStatus.SUBMITTED,
   }),
-  'text-message': TemplateFactory.create({
-    id: 'valid-sms-template',
-    name: 'test-template-mat-sms',
+  emailNotYetSubmitted: TemplateFactory.create({
+    id: uuid(),
+    version: 1,
+    name: 'email-not-yet-submitted_manage-templates-page',
+    message: 'test example message',
+    subject: 'test example subject',
+    templateType: TemplateType.EMAIL,
+    templateStatus: TemplateStatus.NOT_YET_SUBMITTED,
+  }),
+  smsSubmitted: TemplateFactory.create({
+    id: uuid(),
+    name: 'sms-submitted_manage-templates-page',
     message: 'test example message',
     templateType: TemplateType.SMS,
     templateStatus: TemplateStatus.SUBMITTED,
   }),
-  'nhs-app': TemplateFactory.create({
-    id: 'valid-nhs-app-template',
-    name: 'test-template-hat-nhs-app',
+  smsNotYetSubmitted: TemplateFactory.create({
+    id: uuid(),
+    name: 'sms-not-yet-submitted_manage-templates-page',
+    message: 'test example message',
+    templateType: TemplateType.SMS,
+    templateStatus: TemplateStatus.SUBMITTED,
+  }),
+  nhsAppSubmitted: TemplateFactory.create({
+    id: uuid(),
+    name: 'nhs-app-submitted_manage-templates-page',
     message: 'test example message',
     templateType: TemplateType.NHS_APP,
     templateStatus: TemplateStatus.SUBMITTED,
   }),
+  nhsAppNotYetSubmitted: TemplateFactory.create({
+    id: uuid(),
+    name: 'nhs-app-not-yet-submitted_manage-templates-page',
+    message: 'test example message',
+    templateType: TemplateType.NHS_APP,
+    templateStatus: TemplateStatus.NOT_YET_SUBMITTED,
+  }),
 };
 
-test.describe('Message templates page', () => {
+test.describe('Manage templates page', () => {
   const templateStorageHelper = new TemplateStorageHelper(
     Object.values(templates)
   );
@@ -63,7 +87,7 @@ test.describe('Message templates page', () => {
     await assertGoBackLinkNotPresent(props);
   });
 
-  test('should navigate to the Message templates page', async ({
+  test('should navigate to the manage templates page', async ({
     page,
     baseURL,
   }) => {
@@ -76,7 +100,7 @@ test.describe('Message templates page', () => {
     expect(await manageTemplatesPage.createTemplateButton).toBeVisible();
   });
 
-  test('Template item has correct status if it has been has been submitted', async ({
+  test('Submitted template items have correct status indicator', async ({
     page,
     baseURL,
   }) => {
@@ -85,16 +109,33 @@ test.describe('Message templates page', () => {
 
     expect(page.url()).toContain(`${baseURL}/templates/manage-templates`);
 
-    const email = await page.locator('tr:has-text("test-template-cat-email")');
-    await expect(await email.getByText('Submitted')).toBeVisible();
+    const emailSubmitted = await page.locator('tr:has-text("email-submitted_manage-templates-page")');
+    await expect(await emailSubmitted.getByText('Submitted', { exact: true })).toBeVisible();
 
-    const sms = await page.locator('tr:has-text("test-template-mat-sms")');
-    await expect(await sms.getByText('Submitted')).toBeVisible();
+    const smsSubmitted = await page.locator('tr:has-text("sms-submitted_manage-templates-page")');
+    await expect(await smsSubmitted.getByText('Submitted', { exact: true })).toBeVisible();
 
-    const nhsapp = await page.locator(
-      'tr:has-text("test-template-hat-nhs-app")'
-    );
-    await expect(await nhsapp.getByText('Submitted')).toBeVisible();
+    const nhsapp = await page.locator('tr:has-text("nhs-app-submitted_manage-templates-page")');
+    await expect(await nhsapp.getByText('Submitted', { exact: true })).toBeVisible();
+  });
+
+  test('Not Yet Submitted template items have correct status indicator', async ({
+    page,
+    baseURL,
+  }) => {
+    const manageTemplatesPage = new ManageTemplatesPage(page);
+    await manageTemplatesPage.loadPage();
+
+    expect(page.url()).toContain(`${baseURL}/templates/manage-templates`);
+
+    const email = await page.locator('tr:has-text("email-not-yet-submitted_manage-templates-page")');
+    await expect(await email.getByText('Not yet submitted', { exact: true })).toBeVisible();
+
+    const sms = await page.locator('tr:has-text("sms-not-yet-submitted_manage-templates-page")');
+    await expect(await sms.getByText('Not yet submitted', { exact: true })).toBeVisible();
+
+    const nhsapp = await page.locator('tr:has-text("nhs-app-not-yet-submitted_manage-templates-page")');
+    await expect(await nhsapp.getByText('Not yet submitted', { exact: true })).toBeVisible();
   });
 
   test('should navigate to "choose template" page when create template button is clicked', async ({
@@ -115,9 +156,9 @@ test.describe('Message templates page', () => {
   });
 
   // test placeholders:
-  test('Template name link navigation - navigates to preview page', async () => {});
+  test('Template name link navigation - navigates to preview page', async () => { });
 
-  test('Copy link navigation - Navigates user to duplicate template type page ', async () => {});
+  test('Copy link navigation - Navigates user to duplicate template type page ', async () => { });
 
-  test('Delete link navigation - Navigates user to delete template page ', async () => {});
+  test('Delete link navigation - Navigates user to delete template page ', async () => { });
 });
