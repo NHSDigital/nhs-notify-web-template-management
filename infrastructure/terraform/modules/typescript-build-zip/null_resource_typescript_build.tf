2da59624cb54a8a4ff8a591ba34298bc58b744e6
Chris Elliott <chris.elliott17@nhs.net>
2024-12-05 13:29:46 +0000

    Fix typescript build

--- infrastructure/terraform/modules/typescript-build-zip/null_resource_typescript_build.tf
+++ infrastructure/terraform/modules/typescript-build-zip/null_resource_typescript_build.tf
@@ -5,6 +5,6 @@ resource "null_resource" "typescript_build" {
 
   provisioner "local-exec" {
     working_dir = var.source_code_dir
-    command     = "rm -rf ${var.output_dir} && npx esbuild --bundle --minify --sourcemap --target=es2020 --platform=node --outdir=${var.output_dir} ${join(" ", var.entrypoints)}"
+    command     = "rm -rf ${var.output_dir} && npm ci && npx esbuild --bundle --minify --sourcemap --target=es2020 --platform=node --outdir=${var.output_dir} ${join(" ", var.entrypoints)}"
   }
 }
