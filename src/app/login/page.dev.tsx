de441a3051f8fd6c1cd8a96efcf04001cae67cbe
Michael Harrison <michael.harrison31@nhs.net>
2024-12-06 18:01:50 +0000

    CCM-7462: add login functionality

--- src/app/login/page.dev.tsx
+++ src/app/login/page.dev.tsx
@@ -1,7 +1,26 @@
-'use server';
+'use client';
 
-const LoginPage = () => {
-  return <div>Login</div>;
+import React, { Suspense } from 'react';
+import { Amplify } from 'aws-amplify';
+
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import { Redirect } from '@molecules/Redirect/Redirect';
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports, unicorn/prefer-module, import/no-unresolved
+Amplify.configure(require('../../../amplify_outputs.json'), { ssr: true });
+
+const AuthenticatorWrapper = () => {
+  return withAuthenticator(Redirect, {
+    variation: 'default',
+    hideSignUp: true,
+  })({});
 };
 
-export default LoginPage;
+export default function Page() {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <AuthenticatorWrapper />
+    </Suspense>
+  );
+}
