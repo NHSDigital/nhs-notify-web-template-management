1966f12c124e16eadfa04fc1c05900ab25f50327
Michael Harrison <michael.harrison31@nhs.net>
2024-12-06 18:08:44 +0000

    CCM-7462: inline redirect

--- src/app/login/page.dev.tsx
+++ src/app/login/page.dev.tsx
@@ -1,14 +1,28 @@
 'use client';
 
 import React, { Suspense } from 'react';
+import { useSearchParams, redirect, RedirectType } from 'next/navigation';
 import { Amplify } from 'aws-amplify';
-
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import { Redirect } from '@molecules/Redirect/Redirect';
+import path from 'path';
 
 // eslint-disable-next-line @typescript-eslint/no-require-imports, unicorn/prefer-module, import/no-unresolved
 Amplify.configure(require('../../../amplify_outputs.json'), { ssr: true });
+
+const Redirect = () => {
+  const searchParams = useSearchParams();
+
+  const requestDirectPath = searchParams.get('redirect');
+
+  if (!requestDirectPath) {
+    return redirect('/', RedirectType.push);
+  }
+
+  const redirectPath = path.normalize(`/redirect/${requestDirectPath}`);
+
+  return redirect(redirectPath, RedirectType.push);
+};
 
 const AuthenticatorWrapper = () => {
   return withAuthenticator(Redirect, {
