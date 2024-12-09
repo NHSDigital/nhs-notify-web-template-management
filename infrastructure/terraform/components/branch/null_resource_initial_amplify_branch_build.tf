aac87901d7058b97ea0aa16a2639ef944ae4cabe
aidenvaines-bjss <aiden.vaines@bjss.com>
2024-12-09 12:29:05 +0000

    CCM-6413 forcing autobuilds

---
+++ infrastructure/terraform/components/branch/null_resource_initial_amplify_branch_build.tf
@@ -0,0 +1,13 @@
+resource "null_resource" "initial_amplify_branch_build" {
+  # Trigger initial build when the branch is creates. Subsequent commits to the branch would cause builds anyway
+  triggers = {
+    csi                 = local.csi
+    amplify_app_id      = local.app.amplify["id"]
+    amplify_branch_name = module.amplify_branch.name
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "aws amplify start-deployment --app-id ${self.triggers.amplify_app_id} --branch-name ${self.triggers.amplify_branch_name}"
+  }
+}
