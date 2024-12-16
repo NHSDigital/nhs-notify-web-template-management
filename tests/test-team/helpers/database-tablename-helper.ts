d7d6d36375c20e6b01851f15a3aa651b56ad8b92
ben.hansell1 <ben.hansell1@nhs.net>
2024-12-16 14:29:26 +0000

    CCM-7465: get dynamoDB table name from sandbox outputs

--- tests/test-team/helpers/database-tablename-helper.ts
+++ tests/test-team/helpers/database-tablename-helper.ts
@@ -1,63 +1,11 @@
-import {
-  AppSyncClient,
-  paginateListGraphqlApis,
-  GraphqlApi,
-} from '@aws-sdk/client-appsync';
-import { Amplify } from 'aws-amplify';
 import * as fs from 'node:fs';
 
 export class DatabaseTableNameHelper {
-  private readonly _appSyncClient: AppSyncClient;
-
-  private _appApiId?: string;
-
-  constructor() {
-    this._appSyncClient = new AppSyncClient({ region: 'eu-west-2' });
-  }
-
-  private async listGraphqlAPIs() {
-    const apiList: GraphqlApi[] = [];
-
-    const paginator = paginateListGraphqlApis(
-      { client: this._appSyncClient },
-      {}
-    );
-
-    for await (const page of paginator) {
-      const apiData = page.graphqlApis ?? [];
-      apiList.push(...apiData);
-    }
-
-    return apiList;
-  }
-
-  private async getApiId() {
-    if (this._appApiId) {
-      return this._appApiId;
-    }
-
-    const amplifyOutput = JSON.parse(
-      fs.readFileSync('../../frontend/amplify_outputs.json', 'utf8')
-    );
-
-    Amplify.configure(amplifyOutput);
-
-    const graphqlAPIs = await this.listGraphqlAPIs();
-
-    const outputUri = Amplify.getConfig().API?.GraphQL?.endpoint;
-
-    const matchingGraphqlAPI = graphqlAPIs.find(
-      (graphqlAPI) => graphqlAPI.uris?.GRAPHQL === outputUri
-    );
-
-    this._appApiId = matchingGraphqlAPI?.apiId;
-
-    return this._appApiId;
-  }
-
   public async getTemplateStorageTableName() {
-    const appApiId = await this.getApiId();
+    const sandboxOutputs = JSON.parse(
+      fs.readFileSync('../../sandbox_tf_outputs.json', 'utf8')
+    );
 
-    return `TemplateStorage-${appApiId}-NONE`;
+    return sandboxOutputs?.dynamodb_table_templates?.value;
   }
 }
