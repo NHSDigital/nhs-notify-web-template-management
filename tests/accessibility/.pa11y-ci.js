c9896f06f481b4c8a6c5f8ba81333be9611f575f
Chris Elliott <chris.elliott17@nhs.net>
2024-12-17 00:18:12 +0000

    Update accessibility tests

--- tests/accessibility/.pa11y-ci.js
+++ tests/accessibility/.pa11y-ci.js
@@ -37,7 +37,6 @@ const manageTemplatesUrl = `${baseUrl}/manage-templates`;
 
 module.exports = {
   urls: [
-    'http://localhost:3000/some-404'/*
     performCheck({ url: 'http://localhost:3000/some-404', name: '404-test' }),
     performCheck({ url: startUrl, name: 'landing-page' }),
 
@@ -80,7 +79,7 @@ module.exports = {
     performCheck(viewSubmittedEmailTemplatePage(manageTemplatesUrl)),
 
     performCheck({ url: `${baseUrl}/invalid-template`, name: 'invalid-template'}),
-    performCheck({ url: `${baseUrl}/testing/email-template.html`, name: 'email-template'})*/
+    performCheck({ url: `${baseUrl}/testing/email-template.html`, name: 'email-template'})
   ],
   defaults: {
     reporters: [
