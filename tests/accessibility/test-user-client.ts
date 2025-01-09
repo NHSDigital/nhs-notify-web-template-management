5e9a6b8611014dcb66e6cfd286f94857991e41d1
Chris Elliott <chris.elliott17@nhs.net>
2025-01-09 13:13:47 +0000

    Fix CI errors

--- tests/accessibility/test-user-client.ts
+++ tests/accessibility/test-user-client.ts
@@ -5,22 +5,22 @@ import {
     CognitoIdentityProviderClient,
   } from '@aws-sdk/client-cognito-identity-provider';
   import { readFileSync } from 'node:fs';
-  
+
   export class TestUserClient {
     private readonly cognitoClient = new CognitoIdentityProviderClient({
       region: 'eu-west-2',
     });
-  
+
     private readonly userPoolId: string;
-  
+
     constructor(amplifyOutputsPathPrefix = '../..') {
         const amplifyOutputs = JSON.parse(
           readFileSync(`${amplifyOutputsPathPrefix}/amplify_outputs.json`, 'utf8')
         );
-    
+
         this.userPoolId = amplifyOutputs.auth.user_pool_id;
     }
-  
+
     async createTestUser(email: string, password: string) {
       const res = await this.cognitoClient.send(
         new AdminCreateUserCommand({
@@ -39,7 +39,7 @@ import {
           MessageAction: 'SUPPRESS',
         })
       );
-  
+
       await this.cognitoClient.send(
         new AdminSetUserPasswordCommand({
           UserPoolId: this.userPoolId,
@@ -48,16 +48,16 @@ import {
           Permanent: true,
         })
       );
-  
+
       const username = res.User?.Username;
-  
+
       if (!username) {
         throw new Error('Error during user creation');
       }
-  
+
       return username;
     }
-  
+
     async deleteTestUser(email: string) {
       await this.cognitoClient.send(
         new AdminDeleteUserCommand({
