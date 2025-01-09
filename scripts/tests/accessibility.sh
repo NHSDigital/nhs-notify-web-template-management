f853d0cf6a8702b741cbe5cc1960ed85ba3f0858
Chris Elliott <chris.elliott17@nhs.net>
2025-01-09 13:58:15 +0000

    Fix accessibility.sh

--- scripts/tests/accessibility.sh
+++ scripts/tests/accessibility.sh
@@ -4,11 +4,11 @@ set -euo pipefail
 
 npm run create-test-user
 
-# npm run build --prefix frontend
+npm run build --prefix frontend
 
-# npm run app:start --prefix frontend
+npm run app:start --prefix frontend
 
-# npm run app:wait --prefix frontend
+npm run app:wait --prefix frontend
 
 npm run test:accessibility
 
