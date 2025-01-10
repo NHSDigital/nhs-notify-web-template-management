78b44aecd7b59a59ef1e45c489256eda849bf171
Chris Elliott <chris.elliott17@nhs.net>
2025-01-10 13:11:24 +0000

    Fix accessibility tests

---
+++ tests/accessibility/actions/sign-in-page.actions.js
@@ -0,0 +1,14 @@
+const { readFileSync } = require('node:fs');
+
+const { email, password } = JSON.parse(readFileSync('./auth.json', 'utf8'));
+
+const signInPageActions = [
+  'wait for element button[type="submit"] to be visible',
+  `set field input[type="text"] to ${email}`,
+  `set field input[type="password"] to ${password}`,
+  'click element button[type="submit"]',
+];
+
+module.exports = {
+    signInPageActions,
+};
--- tests/accessibility/actions/view-submitted-text-message-template.actions.js
+++ tests/accessibility/actions/view-submitted-text-message-template.actions.js
@@ -1,4 +1,7 @@
+const { signInPageActions } = require('./sign-in-page.actions');
+
 const pageActions = [
+  ...signInPageActions,
   'wait for element a[href*="view-submitted-text-message-template"] to be visible',
   'click element a[href*="view-submitted-text-message-template"]',
   'wait for element #preview-heading-message to be visible',
