4eedac62ea197eb38208a08adfb8431dffe4e4e7
aidenvaines-bjss <aiden.vaines@bjss.com>
2025-02-04 14:51:43 +0000

    CCM-8501 backup permissions

--- infrastructure/terraform/components/acct/iam_policy_github_deploy_overload.tf
+++ infrastructure/terraform/components/acct/iam_policy_github_deploy_overload.tf
@@ -18,6 +18,7 @@ data "aws_iam_policy_document" "github_deploy" {
       "amplify:*",
       "apigateway:*",
       "appsync:*",
+      "backup:*",
       "cloudformation:*",
       "cognito-idp:*",
       "ses:*",
