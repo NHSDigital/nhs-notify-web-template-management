e28a3cb9f2b29a9c2cd0153ac6bbe129b52ec3f6
Chris Elliott <chris.elliott17@nhs.net>
2025-02-12 11:58:44 +0000

    Changes in response to review comments

--- infrastructure/terraform/components/app/ssm_parameter_csrf_secret.tf
+++ infrastructure/terraform/components/app/ssm_parameter_csrf_secret.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "csrf_secret" {
   name        = "/${local.csi}/csrf_secret"
-  description = "The Basic Auth password used for the amplify app. This parameter is sourced from Github Environment variables"
+  description = "cryptographic secret used for CSRF token generation"
 
   type  = "SecureString"
   value = var.CSRF_SECRET
