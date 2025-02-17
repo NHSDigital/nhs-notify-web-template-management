9ad5dcb44bb34196d9ae353e6992c2d5c76a8650
ben.hansell1 <ben.hansell1@nhs.net>
2025-02-17 16:12:58 +0000

    CCM-8603: add empty onClick handler for buttons which invokes the NHS Button preventDblClick
    CCM-8603: remove waitFor

--- frontend/src/components/atoms/NHSNotifyButton/NHSNotifyButton.tsx
+++ frontend/src/components/atoms/NHSNotifyButton/NHSNotifyButton.tsx
@@ -1,15 +1,20 @@
 'use client';
 
 import { Button } from 'nhsuk-react-components';
-import { ButtonType } from './button.types';
 
 export function NHSNotifyButton({
   children,
   href,
   ...rest
-}: ButtonType & React.ComponentProps<typeof Button>) {
+}: React.ComponentProps<typeof Button>) {
   return (
-    <Button preventDoubleClick {...rest} href={href} data-testid='link-button'>
+    <Button
+      preventDoubleClick
+      debounceTimeout={10_000}
+      onClick={() => {}}
+      href={href}
+      {...rest}
+    >
       {children}
     </Button>
   );
--- frontend/src/components/forms/DeleteTemplate/DeleteTemplate.tsx
+++ frontend/src/components/forms/DeleteTemplate/DeleteTemplate.tsx
@@ -4,9 +4,9 @@ import { FC } from 'react';
 import { useFormState } from 'react-dom';
 import { ChannelTemplate } from 'nhs-notify-web-template-management-utils';
 import { deleteTemplatePageContent } from '@content/content';
-import { Button } from 'nhsuk-react-components';
 import { NHSNotifyMain } from '@atoms/NHSNotifyMain/NHSNotifyMain';
 import { NHSNotifyFormWrapper } from '@molecules/NHSNotifyFormWrapper/NHSNotifyFormWrapper';
+import { NHSNotifyButton } from '@atoms/NHSNotifyButton/NHSNotifyButton';
 import {
   deleteTemplateYesAction,
   deleteTemplateNoAction,
@@ -37,14 +37,16 @@ export const DeleteTemplate: FC<DeleteTemplateProps> = ({ template }) => {
               style: { display: 'inline' },
             }}
           >
-            <Button preventDoubleClick secondary>{noButtonText}</Button>
+            <NHSNotifyButton secondary>{noButtonText}</NHSNotifyButton>
           </NHSNotifyFormWrapper>
           <NHSNotifyFormWrapper
             action={action}
             formAttributes={{ style: { display: 'inline' } }}
             formId='delete-template-yes'
           >
-            <Button preventDoubleClick className='nhsuk-button--warning'>{yesButtonText}</Button>
+            <NHSNotifyButton className='nhsuk-button--warning'>
+              {yesButtonText}
+            </NHSNotifyButton>
           </NHSNotifyFormWrapper>
         </div>
       </div>
