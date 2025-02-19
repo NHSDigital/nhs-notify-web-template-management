4a3105c4f3213ceac8fd99f1e641e3d6a8f557ae
Alex Nuttall <alex.nuttall1@nhs.net>
2025-02-19 14:30:39 +0000

    CCM-8580: remove unused extra enum

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,10 +1,2 @@
-import { TemplateType } from 'nhs-notify-backend-client';
-
 export const MAX_SMS_CHARACTER_LENGTH = 918 as const;
 export const MAX_EMAIL_CHARACTER_LENGTH = 100_000 as const;
-
-export enum TemplateTypeDigital {
-  NHS_APP = TemplateType.NHS_APP,
-  SMS = TemplateType.SMS,
-  EMAIL = TemplateType.EMAIL,
-}
