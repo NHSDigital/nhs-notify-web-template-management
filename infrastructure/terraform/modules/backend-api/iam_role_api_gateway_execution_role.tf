b045e5b87388e9329e09b0bab394f0c1da53743c
aidenvaines-bjss <aiden.vaines@bjss.com>
2025-03-07 09:38:50 +0000

    CCM-8752 Replace inline policies with attached policies

--- infrastructure/terraform/modules/backend-api/iam_role_api_gateway_execution_role.tf
+++ infrastructure/terraform/modules/backend-api/iam_role_api_gateway_execution_role.tf
@@ -7,13 +7,16 @@ resource "aws_iam_role" "api_gateway_execution_role" {
 resource "aws_iam_role_policy_attachment" "cloudwatch_logs" {
   role       = aws_iam_role.api_gateway_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs"
-
 }
 
-resource "aws_iam_role_policy" "api_gateway_execution_policy" {
-  role   = aws_iam_role.api_gateway_execution_role.name
+resource "aws_iam_policy" "api_gateway_execution_policy" {
   name   = "${local.csi}-apig-execution-policy"
   policy = data.aws_iam_policy_document.api_gateway_execution_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "api_gateway_execution" {
+  role       = aws_iam_role.api_gateway_execution_role.name
+  policy_arn = aws_iam_policy.api_gateway_execution_policy.arn
 }
 
 data "aws_iam_policy_document" "api_gateway_service_trust_policy" {
@@ -34,7 +37,6 @@ data "aws_iam_policy_document" "api_gateway_service_trust_policy" {
     }
   }
 }
-
 
 data "aws_iam_policy_document" "api_gateway_execution_policy" {
   statement {
