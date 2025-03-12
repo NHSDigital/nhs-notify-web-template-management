d7bc7431d9adffdd5f057b8fac165cf2f000f9ca
Michael Harrison <michael.harrison31@nhs.net>
2025-03-12 12:25:35 +0000

    CCM-8744: sync global signout

--- frontend/src/app/auth/signout/page.dev.tsx
+++ frontend/src/app/auth/signout/page.dev.tsx
@@ -2,18 +2,19 @@
 
 import React, { Suspense, useEffect } from 'react';
 import { useAuthenticator } from '@aws-amplify/ui-react';
+import { signOut } from 'aws-amplify/auth';
 import JsCookie from 'js-cookie';
 import { NHSNotifyMain } from '@atoms/NHSNotifyMain/NHSNotifyMain';
 
 export const SignOut = ({ children }: { children: React.ReactNode }) => {
-  const { signOut, authStatus } = useAuthenticator((ctx) => [ctx.authStatus]);
+  const { authStatus } = useAuthenticator((ctx) => [ctx.authStatus]);
 
   useEffect(() => {
     if (authStatus === 'authenticated') {
       signOut({ global: true });
       JsCookie.remove('csrf_token');
     }
-  }, [authStatus, signOut]);
+  }, [authStatus]);
 
   return (
     <NHSNotifyMain>
