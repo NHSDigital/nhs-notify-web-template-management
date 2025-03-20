b412884be284f64ddde6122921a29feffac4829a
Michael Harrison <michael.harrison31@nhs.net>
2025-03-20 13:19:45 +0000

    CCM-8744: force refresh access tokens in middleware (#337)

--- frontend/src/__tests__/components/molecules/NHSNotifyFormWrapper.test.tsx
+++ frontend/src/__tests__/components/molecules/NHSNotifyFormWrapper.test.tsx
@@ -4,10 +4,13 @@ import {
   csrfServerAction,
 } from '@molecules/NHSNotifyFormWrapper/NHSNotifyFormWrapper';
 import { render } from '@testing-library/react';
-import { verifyCsrfTokenFull } from '@utils/csrf-utils';
+import { redirect } from 'next/navigation';
+import { verifyFormCsrfToken } from '@utils/csrf-utils';
+
+jest.mock('next/navigation');
 
 jest.mock('@utils/csrf-utils', () => ({
-  verifyCsrfTokenFull: jest.fn(),
+  verifyFormCsrfToken: jest.fn(),
 }));
 
 test('Renders back button', () => {
@@ -27,8 +30,9 @@ describe('csrfServerAction', () => {
     expect(action).toEqual('/action');
   });
 
-  test('server action', async () => {
-    const mockAction = jest.fn(() => 'response');
+  test('server action with valid csrf check', async () => {
+    jest.mocked(verifyFormCsrfToken).mockResolvedValueOnce(true);
+    const mockAction = jest.fn();
     const action = csrfServerAction(mockAction);
 
     if (typeof action === 'string') {
@@ -38,7 +42,25 @@ describe('csrfServerAction', () => {
     const mockFormData = mockDeep<FormData>();
     await action(mockFormData);
 
-    expect(verifyCsrfTokenFull).toHaveBeenCalledWith(mockFormData);
+    expect(verifyFormCsrfToken).toHaveBeenCalledWith(mockFormData);
     expect(mockAction).toHaveBeenCalledWith(mockFormData);
+  });
+
+  test('server action with failed csrf check', async () => {
+    jest.mocked(verifyFormCsrfToken).mockResolvedValueOnce(false);
+
+    const mockAction = jest.fn();
+    const action = csrfServerAction(mockAction);
+
+    if (typeof action === 'string') {
+      throw new TypeError('Expected server action');
+    }
+
+    const mockFormData = mockDeep<FormData>();
+    await action(mockFormData);
+
+    expect(verifyFormCsrfToken).toHaveBeenCalledWith(mockFormData);
+    expect(redirect).toHaveBeenCalledWith('/auth/signout');
+    expect(mockAction).not.toHaveBeenCalled();
   });
 });
