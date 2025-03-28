f4134e46ba740d28f7e970e125164ff96b50387f
Alex Nuttall <alex.nuttall1@nhs.net>
2025-03-28 09:53:47 +0000

    CCM-9692: adding pa11y

--- tests/accessibility/pa11y-teardown.ts
+++ tests/accessibility/pa11y-teardown.ts
@@ -2,8 +2,10 @@ import { readFileSync } from 'node:fs';
 import { TestUserClient } from './test-user-client';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DeleteCommand, DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+import { BackendConfigHelper } from 'nhs-notify-web-template-management-util-backend-config';
+import path from 'node:path';
 
-const { email, templateId, userId } = JSON.parse(
+const { email, templateIds, userId } = JSON.parse(
   readFileSync('./pa11y-fixtures.json', 'utf8')
 );
 
@@ -12,17 +14,25 @@ const teardown = async () => {
     new DynamoDBClient({ region: 'eu-west-2' })
   );
 
-  await ddbDocClient.send(
-    new DeleteCommand({
-      TableName: process.env.TEMPLATES_TABLE_NAME,
-      Key: {
-        owner: userId,
-        id: templateId,
-      },
-    })
+  const backendConfig = BackendConfigHelper.fromTerraformOutputsFile(
+    path.join(__dirname, '..', '..', 'sandbox_tf_outputs.json')
   );
 
-  await new TestUserClient('./frontend').deleteTestUser(email);
+  await Promise.all(
+    Object.values(templateIds).map((id) =>
+      ddbDocClient.send(
+        new DeleteCommand({
+          TableName: backendConfig.templatesTableName,
+          Key: {
+            owner: userId,
+            id,
+          },
+        })
+      )
+    )
+  );
+
+  await new TestUserClient(backendConfig.userPoolId).deleteTestUser(email);
 };
 
 teardown();
