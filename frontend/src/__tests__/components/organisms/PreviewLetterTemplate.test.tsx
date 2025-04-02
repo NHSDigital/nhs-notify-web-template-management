96b15555db9da32b9d568af59485ff7b6411d64b
Alex Nuttall <alex.nuttall1@nhs.net>
2025-04-02 16:12:01 +0100

    CCM-8993: fix unused import

--- frontend/src/__tests__/components/organisms/PreviewLetterTemplate.test.tsx
+++ frontend/src/__tests__/components/organisms/PreviewLetterTemplate.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { PreviewLetterTemplate } from '@organisms/PreviewLetterTemplate/PreviewLetterTemplate';
 
 describe('PreviewLetterTemplate component', () => {
