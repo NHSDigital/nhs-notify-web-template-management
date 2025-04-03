a85309e9eb5cf256861aa0a6fa72e88946ef4a7e
Alex Nuttall <alex.nuttall1@nhs.net>
2025-04-03 16:33:54 +0100

    CCM-8637: init

---
+++ lambdas/sftp-letters/jest.config.ts
@@ -0,0 +1,6 @@
+import type { Config } from 'jest';
+import { baseJestConfig } from 'nhs-notify-web-template-management-utils'; // eslint-disable-line no-restricted-exports
+
+const config: Config = { ...baseJestConfig, testEnvironment: 'node' };
+
+export default config;
