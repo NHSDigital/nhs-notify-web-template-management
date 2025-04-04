191b6222d1583b1c36aee537a82af70d078b0cd1
Alex Nuttall <alex.nuttall1@nhs.net>
2025-04-04 11:05:31 +0100

    adding handler

--- lambdas/sftp-letters/src/send-proof.ts
+++ lambdas/sftp-letters/src/send-proof.ts
@@ -1,6 +1,4 @@
-import type { SQSRecord } from 'aws-lambda';
+import { createHandler } from './apis/send-handler';
+import { createContainer } from './container';
 
-export const handler = async (records: SQSRecord[]) => {
-  console.log(records);
-  throw new Error('ohno');
-};
+export const handler = createHandler(createContainer());
