0cca777ad4501e3dbcca23eb884e754661278293
Alex Nuttall <alex.nuttall1@nhs.net>
2025-04-10 16:18:59 +0100

    upgrade SQS

--- infrastructure/terraform/modules/backend-api/module_sqs_sftp_upload_queue.tf
+++ infrastructure/terraform/modules/backend-api/module_sqs_sftp_upload_queue.tf
@@ -1,5 +1,5 @@
 module "sftp_upload_queue" {
-  source = "git::https://github.com/NHSDigital/nhs-notify-shared-modules.git//infrastructure/modules/sqs?ref=80b61143ffd21251b6e13eb5ea154fbc0e74b1b8"
+  source = "git::https://github.com/NHSDigital/nhs-notify-shared-modules.git//infrastructure/modules/sqs?ref=v1.0.14"
 
   aws_account_id = var.aws_account_id
   component      = var.component
