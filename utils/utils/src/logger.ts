09dbd96914dd070a7e4fa5b67969832898107f51
Michael Harrison <michael.harrison31@nhs.net>
2025-04-11 10:21:44 +0100

    CCM-8574: log error cause

--- utils/utils/src/logger.ts
+++ utils/utils/src/logger.ts
@@ -4,7 +4,7 @@ const { combine, timestamp, json, errors } = winston.format;
 
 export const logger = winston.createLogger({
   level: 'info',
-  format: combine(errors({ stack: true }), timestamp(), json()),
+  format: combine(errors({ stack: true, cause: true }), timestamp(), json()),
   transports: [
     new winston.transports.Stream({
       stream: process.stdout,
