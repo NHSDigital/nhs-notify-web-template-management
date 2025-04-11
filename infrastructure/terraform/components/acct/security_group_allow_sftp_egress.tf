a22b3c7ae0a16c8619ba904db7b10e993ddbba5b
Alex Nuttall <alex.nuttall1@nhs.net>
2025-04-11 15:17:19 +0100

    add sg

---
+++ infrastructure/terraform/components/acct/security_group_allow_sftp_egress.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "allow_sftp_egress" {
+  name        = "${local.csi}-sftp-egress"
+  vpc_id      = module.vpc.vpc_id
+  description = "Security group for allowing outbound traffic to SFTP"
+
+  tags = {
+    Name = "${local.csi}-sftp-egress"
+  }
+}
+
+resource "aws_security_group_rule" "allow_sftp_egress_ssh" {
+  description       = "Allow SFTP egress within VPC on port 22"
+  type              = "egress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.allow_sftp_egress.id
+}
+
+resource "aws_security_group_rule" "allow_sftp_egress_https" {
+  description       = "Allow SFTP egress within VPC on port 443"
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.allow_sftp_egress.id
+}
