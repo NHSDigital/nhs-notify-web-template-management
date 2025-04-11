9b10c62be0e828724298313256aac0ead3ac9f9b
Alex Nuttall <alex.nuttall1@nhs.net>
2025-04-11 16:44:27 +0100

    entity update builder tests

--- utils/entity-update-command-builder/jest.config.ts
+++ utils/entity-update-command-builder/jest.config.ts
@@ -1,6 +1,10 @@
 import type { Config } from 'jest';
 import { baseJestConfig } from 'nhs-notify-web-template-management-utils'; // eslint-disable-line no-restricted-exports
 
-const config: Config = { ...baseJestConfig, testEnvironment: 'node' };
+const config: Config = {
+  ...baseJestConfig,
+  testEnvironment: 'node',
+  testPathIgnorePatterns: [],
+};
 
 export default config;
