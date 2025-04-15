e3f43dafd6725b946832902f9195e2a69e2516c4
Alex Nuttall <alex.nuttall1@nhs.net>
2025-04-15 14:35:09 +0100

    descriptions - rm not relevant dns refs

--- infrastructure/terraform/modules/backend-api/variables.tf
+++ infrastructure/terraform/modules/backend-api/variables.tf
@@ -97,5 +97,5 @@ variable "letter_suppliers" {
 
 variable "parent_acct_environment" {
   type        = string
-  description = "Name of the environment responsible for the acct resources used, affects things like DNS zone. Useful for named dev environments"
+  description = "Name of the environment responsible for the acct resources used"
 }
