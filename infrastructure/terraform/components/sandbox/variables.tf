e5eb6556b474888cb874eda728f40173e9afd910
Alex Nuttall <alex.nuttall1@nhs.net>
2025-04-15 14:35:09 +0100

    descriptions - rm not relevant dns refs

--- infrastructure/terraform/components/sandbox/variables.tf
+++ infrastructure/terraform/components/sandbox/variables.tf
@@ -49,7 +49,7 @@ variable "default_tags" {
 
 variable "parent_acct_environment" {
   type        = string
-  description = "Name of the environment responsible for the acct resources used, affects things like DNS zone. Useful for named dev environments"
+  description = "Name of the environment responsible for the acct resources used"
   default     = "main"
 }
 
