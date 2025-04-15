f6f45462e466b98a670ad5fc3016f7a0265b6a75
Michael Harrison <michael.harrison31@nhs.net>
2025-04-15 16:39:50 +0100

    CCM-8574: explicit dependency

--- infrastructure/terraform/modules/lambda_layer/lambda_layer_version_main.tf
+++ infrastructure/terraform/modules/lambda_layer/lambda_layer_version_main.tf
@@ -1,4 +1,5 @@
 resource "aws_lambda_layer_version" "lambda_layer" {
+  depends_on          = [data.archive_file.zip]
   layer_name          = var.name
   description         = var.description
   filename            = data.archive_file.zip.output_path
