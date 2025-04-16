38f640dff178c0b826d8c4fadd496519e48d30f5
Chris Elliott <chris.elliott17@nhs.net>
2025-04-16 14:32:32 +0100

    CCM-8861: Create all events

--- infrastructure/terraform/modules/backend-api/cloudwatch_event_rule_sftp_poll.tf
+++ infrastructure/terraform/modules/backend-api/cloudwatch_event_rule_sftp_poll.tf
@@ -1,12 +1,14 @@
 resource "aws_cloudwatch_event_rule" "sftp_poll" {
-  for_each = { for k, v in var.letter_suppliers : k => v if v.polling_enabled }
+  for_each = var.letter_suppliers
 
   name                = "${local.csi}-sftp-poll-${lower(each.key)}"
   schedule_expression = "rate(1 hour)" # Runs at the top of every hour
+
+  state = each.value.polling_enabled ? "ENABLED" : "DISABLED"
 }
 
 resource "aws_cloudwatch_event_target" "sftp_poll" {
-  for_each = { for k, v in var.letter_suppliers : k => v if v.polling_enabled }
+  for_each = var.letter_suppliers
   rule     = aws_cloudwatch_event_rule.sftp_poll[each.key].name
   arn      = module.lambda_sftp_poll.function_arn
 
@@ -16,7 +18,7 @@ resource "aws_cloudwatch_event_target" "sftp_poll" {
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch" {
-  for_each      = { for k, v in var.letter_suppliers : k => v if v.polling_enabled }
+  for_each      = var.letter_suppliers
   statement_id  = "AllowExecutionFromCloudWatch${each.key}"
   action        = "lambda:InvokeFunction"
   function_name = module.lambda_sftp_poll.function_name
