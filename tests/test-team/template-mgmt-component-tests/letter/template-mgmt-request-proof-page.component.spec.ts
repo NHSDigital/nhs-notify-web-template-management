bab0fbdbca847cdba21137a0d982a38565ef9b4c
Alex Nuttall <alex.nuttall1@nhs.net>
2025-04-25 12:53:17 +0100

    fix unused vars

--- tests/test-team/template-mgmt-component-tests/letter/template-mgmt-request-proof-page.component.spec.ts
+++ tests/test-team/template-mgmt-component-tests/letter/template-mgmt-request-proof-page.component.spec.ts
@@ -1,13 +1,10 @@
 import { test, expect } from '@playwright/test';
 import { TemplateStorageHelper } from '../../helpers/db/template-storage-helper';
 import { TemplateFactory } from '../../helpers/factories/template-factory';
-import { Template } from '../../helpers/types';
 import {
   createAuthHelper,
   TestUserId,
 } from '../../helpers/auth/cognito-auth-helper';
-import { TemplateMgmtPreviewLetterPage } from '../../pages/letter/template-mgmt-preview-letter-page';
-import { TemplateMgmtSubmitLetterPage } from '../../pages/letter/template-mgmt-submit-letter-page';
 import { TemplateMgmtRequestProofPage } from '../../pages/template-mgmt-request-proof-page';
 
 async function createTemplates() {
