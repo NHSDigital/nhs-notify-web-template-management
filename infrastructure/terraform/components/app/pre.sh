c6117749d7920fc7a38de1f8c7f75138232cae39
Alex Nuttall <alex.nuttall1@nhs.net>
2025-04-28 13:49:09 +0100

    always run build

--- infrastructure/terraform/components/app/pre.sh
+++ infrastructure/terraform/components/app/pre.sh
@@ -2,9 +2,4 @@ npm ci
 
 npm run generate-dependencies --workspaces --if-present
 
-top_level=$(git rev-parse --show-toplevel)
-pdfjs_zip=${top_level}/lambdas/layers/pdfjs/dist/layer/pdfjs-layer.zip
-
-if [ ! -f "$pdfjs_zip" ]; then
-    ${top_level}/lambdas/layers/pdfjs/build.sh
-fi
+$(git rev-parse --show-toplevel)/lambdas/layers/pdfjs/build.sh
