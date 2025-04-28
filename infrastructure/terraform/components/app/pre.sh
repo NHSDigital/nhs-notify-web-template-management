3f7fa27940d12c26ff0bc8db9494e6c4fb0f0243
Alex Nuttall <alex.nuttall1@nhs.net>
2025-04-28 10:52:31 +0100

    rm freshness check

--- infrastructure/terraform/components/app/pre.sh
+++ infrastructure/terraform/components/app/pre.sh
@@ -5,6 +5,6 @@ npm run generate-dependencies --workspaces --if-present
 top_level=$(git rev-parse --show-toplevel)
 pdfjs_zip=${top_level}/lambdas/layers/pdfjs/dist/layer/pdfjs-layer.zip
 
-if [ ! -f "$pdfjs_zip" ] || ! find "$pdfjs_zip" -mmin -30 -print -quit | grep -q '^'; then
+if [ ! -f "$pdfjs_zip" ]; then
     ${top_level}/lambdas/layers/pdfjs/build.sh
 fi
