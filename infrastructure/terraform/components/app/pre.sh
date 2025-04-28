325dad52e4acbcc1c3af77ebe7fb97d5d4a815e6
Alex Nuttall <alex.nuttall1@nhs.net>
2025-04-28 10:36:46 +0100

    fix rebuild condition

--- infrastructure/terraform/components/app/pre.sh
+++ infrastructure/terraform/components/app/pre.sh
@@ -3,8 +3,8 @@ npm ci
 npm run generate-dependencies --workspaces --if-present
 
 top_level=$(git rev-parse --show-toplevel)
-pdfjs_zip=${top_level}/lambdas/layers/pdfjs/dist/pdfjs-layer.zip
+pdfjs_zip=${top_level}/lambdas/layers/pdfjs/dist/layer/pdfjs-layer.zip
 
-if ! find "$pdfjs_zip" -mmin -60 -print -quit | grep -q '^'; then
-    ${top_level}/lamdbas/layers.pdfjs/build.sh
+if [ ! -f "$pdfjs_zip" ] || ! find "$pdfjs_zip" -mmin -30 -print -quit | grep -q '^'; then
+    ${top_level}/lambdas/layers/pdfjs/build.sh
 fi
