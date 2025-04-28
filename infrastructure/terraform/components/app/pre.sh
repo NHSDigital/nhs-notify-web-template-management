f4460ec9e9a3cb29af7f86fa09407ddfca24fd98
Alex Nuttall <alex.nuttall1@nhs.net>
2025-04-28 07:59:45 +0100

    logging lambdas

--- infrastructure/terraform/components/app/pre.sh
+++ infrastructure/terraform/components/app/pre.sh
@@ -1,6 +1,7 @@
 npm ci
 
 npm run generate-dependencies --workspaces --if-present
+ls -la ./lambdas
 ls -la "lambdas/layers/"
 
 ls -la "lambdas/layers/pdfjs/" 2>/dev/null || echo "pdfjs directory not found"
