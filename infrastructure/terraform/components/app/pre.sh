2f79984d10d493703dc32ec8ca05f4806b040ed3
Alex Nuttall <alex.nuttall1@nhs.net>
2025-04-28 10:28:39 +0100

    invert condition

--- infrastructure/terraform/components/app/pre.sh
+++ infrastructure/terraform/components/app/pre.sh
@@ -5,6 +5,6 @@ npm run generate-dependencies --workspaces --if-present
 top_level=$(git rev-parse --show-toplevel)
 pdfjs_zip=${top_level}/lambdas/layers/pdfjs/dist/pdfjs-layer.zip
 
-if find "$pdfjs_zip" -mmin -60 -print -quit | grep -q '^'; then
+if ! find "$pdfjs_zip" -mmin -60 -print -quit | grep -q '^'; then
     ${top_level}/lamdbas/layers.pdfjs/build.sh
 fi
