197389a2bd84eef672daa8cf4b2d89c3df58b4aa
Alex Nuttall <alex.nuttall1@nhs.net>
2025-04-28 10:21:13 +0100

    skip rebuilding zip

--- infrastructure/terraform/components/app/pre.sh
+++ infrastructure/terraform/components/app/pre.sh
@@ -2,4 +2,9 @@ npm ci
 
 npm run generate-dependencies --workspaces --if-present
 
-$(git rev-parse --show-toplevel)/lambdas/layers/pdfjs/build.sh
+top_level=$(git rev-parse --show-toplevel)
+pdfjs_zip=${top_level}/lambdas/layers/pdfjs/dist/pdfjs-layer.zip
+
+if find "$pdfjs_zip" -mmin -60 -print -quit | grep -q '^'; then
+    ${top_level}/lamdbas/layers.pdfjs/build.sh
+fi
--- infrastructure/terraform/modules/backend-api/lambda_layer_pdfjs.tf
+++ infrastructure/terraform/modules/backend-api/lambda_layer_pdfjs.tf
@@ -2,5 +2,6 @@ resource "aws_lambda_layer_version" "lambda_layer_pdfjs" {
   layer_name          = "${local.csi}-nodejs20-pdfjs-dist"
   description         = "pdfjs-dist dependencies for Node.js v20"
   filename            = local.pdfjs_layer_zip
+  source_code_hash    = filebase64sha256(local.pdfjs_layer_zip)
   compatible_runtimes = ["nodejs20.x"]
 }
