7dc7035f81acd1d5db127402ae914589a6065a98
Alex Nuttall <alex.nuttall1@nhs.net>
2025-04-28 08:05:47 +0100

    try git rev-parse

--- infrastructure/terraform/components/app/pre.sh
+++ infrastructure/terraform/components/app/pre.sh
@@ -1,8 +1,4 @@
 npm ci
 
 npm run generate-dependencies --workspaces --if-present
-ls -la ./lambdas
-ls -la "lambdas/layers/"
-
-ls -la "lambdas/layers/pdfjs/" 2>/dev/null || echo "pdfjs directory not found"
-./lambdas/layers/pdfjs/build.sh
+$(git rev-parse --show-toplevel)/lambdas/layers/pdfjs/build.sh
