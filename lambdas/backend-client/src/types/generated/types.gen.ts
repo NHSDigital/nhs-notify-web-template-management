754c3e13fd783073913988945ab7240feaa6ee08
Nicki Derrick <nicki.derrick1@nhs.net>
2025-05-06 11:49:53 +0100

    CCM-9118 Add new status

--- lambdas/backend-client/src/types/generated/types.gen.ts
+++ lambdas/backend-client/src/types/generated/types.gen.ts
@@ -5,6 +5,7 @@ export type TemplateType = 'NHS_APP' | 'EMAIL' | 'SMS' | 'LETTER';
 export type TemplateStatus =
   | 'DELETED'
   | 'NOT_YET_SUBMITTED'
+  | 'PENDING_PROOF'
   | 'PENDING_PROOF_REQUEST'
   | 'PENDING_UPLOAD'
   | 'PENDING_VALIDATION'
