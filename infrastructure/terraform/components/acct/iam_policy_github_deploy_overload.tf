2e587da10eea4e68f8b7f6a8f9f662bc8cb340a5
jamesthompson26-nhs <115219893+jamesthompson26-nhs@users.noreply.github.com>
2025-05-09 11:54:18 +0100

    Feature/ccm 8197 cross account observability (#449)

--- infrastructure/terraform/components/acct/iam_policy_github_deploy_overload.tf
+++ infrastructure/terraform/components/acct/iam_policy_github_deploy_overload.tf
@@ -23,6 +23,8 @@ data "aws_iam_policy_document" "github_deploy" {
       "cloudformation:*",
       "cognito-idp:*",
       "firehose:*",
+      "logs:*",
+      "oam:*",
       "pipes:*",
       "ses:*",
       "sns:*",
