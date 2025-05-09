ccedd2bd56586110defa052d05b6cff1fe7608f0
ben.hansell1 <ben.hansell1@nhs.net>
2025-05-09 15:35:03 +0100

    CCM-9873: remove last vestigates of having CDN in sandbox

--- infrastructure/terraform/components/sandbox/module_backend_api.tf
+++ infrastructure/terraform/components/sandbox/module_backend_api.tf
@@ -9,8 +9,7 @@ module "backend_api" {
   group                       = var.group
   csi                         = local.csi
   log_retention_in_days       = var.log_retention_in_days
-  parent_acct_environment     = var.parent_acct_environment
-
+  parent_acct_environment     = "main"
   cloudfront_distribution_arn = "WIP"
 
   cognito_config = {
