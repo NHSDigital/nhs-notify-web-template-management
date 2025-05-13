a2b9defc13ad2ae6a35baf7cad438f7167324753
Alex Nuttall <alex.nuttall1@nhs.net>
2025-05-13 10:46:21 +0100

    rm sandbox parent acct env var

--- infrastructure/terraform/components/sandbox/module_backend_api.tf
+++ infrastructure/terraform/components/sandbox/module_backend_api.tf
@@ -1,15 +1,15 @@
 module "backend_api" {
   source = "../../modules/backend-api"
 
-  project                     = var.project
-  environment                 = var.environment
-  component                   = var.component
-  aws_account_id              = var.aws_account_id
-  region                      = var.region
-  group                       = var.group
-  csi                         = local.csi
-  log_retention_in_days       = var.log_retention_in_days
-  parent_acct_environment     = var.parent_acct_environment
+  project                 = var.project
+  environment             = var.environment
+  component               = var.component
+  aws_account_id          = var.aws_account_id
+  region                  = var.region
+  group                   = var.group
+  csi                     = local.csi
+  log_retention_in_days   = var.log_retention_in_days
+  parent_acct_environment = "main"
 
   cognito_config = {
     USER_POOL_ID        = aws_cognito_user_pool.sandbox.id
