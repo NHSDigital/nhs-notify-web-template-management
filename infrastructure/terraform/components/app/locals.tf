be891c3afa5682a1c25bd36600f024e701152fc5
Alex Nuttall <alex.nuttall1@nhs.net>
2025-05-13 13:55:39 +0100

    dont do abspath twice

--- infrastructure/terraform/components/app/locals.tf
+++ infrastructure/terraform/components/app/locals.tf
@@ -3,5 +3,5 @@ locals {
   root_domain_name             = "${var.environment}.${local.acct.dns_zone["name"]}"
 
   repo_root               = abspath("${path.module}/../../../..")
-  lambdas_source_code_dir = abspath("${local.repo_root}/lambdas")
+  lambdas_source_code_dir = "${local.repo_root}/lambdas"
 }
