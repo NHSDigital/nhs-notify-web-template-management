18d94170394f33973fd37884c0e4461b49269961
Alex Nuttall <alex.nuttall1@nhs.net>
2025-05-13 08:09:32 +0100

    cleanup

--- infrastructure/terraform/components/acct/module_s3bucket_access_logs.tf
+++ infrastructure/terraform/components/acct/module_s3bucket_access_logs.tf
@@ -1,5 +1,5 @@
 module "s3bucket_access_logs" {
-  source = "git::https://github.com/NHSDigital/nhs-notify-shared-modules.git//infrastructure/modules/s3bucket?ref=v=1.0.9"
+  source = "git::https://github.com/NHSDigital/nhs-notify-shared-modules.git//infrastructure/modules/s3bucket?ref=v1.0.9"
 
   name = "access-logs"
 
