6d1fb55acea4134513328fa5deb00567e83137e6
Alex Nuttall <alex.nuttall1@nhs.net>
2025-05-14 19:13:43 +0100

    cleanup

--- infrastructure/terraform/modules/lambda-function/outputs.tf
+++ infrastructure/terraform/modules/lambda-function/outputs.tf
@@ -1,9 +1,6 @@
 output "function_arn" {
   value = aws_lambda_function.main.arn
 }
-output "function_qualified_arn" {
-  value = aws_lambda_function.main.qualified_arn
-}
 output "function_name" {
   value = aws_lambda_function.main.function_name
 }
