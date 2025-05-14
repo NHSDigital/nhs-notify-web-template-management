81c55ad308c968a5b7be5945f9a60eeaec48d8c1
Alex Nuttall <alex.nuttall1@nhs.net>
2025-05-14 17:11:54 +0100

    try fix packaging

--- infrastructure/terraform/components/app/locals.tf
+++ infrastructure/terraform/components/app/locals.tf
@@ -2,6 +2,5 @@ locals {
   cloudfront_files_domain_name = "files.${local.root_domain_name}"
   root_domain_name             = "${var.environment}.${local.acct.dns_zone["name"]}"
 
-  lambdas_dir             = "../../../../lambdas"
-  lambdas_source_code_dir = abspath("${path.module}/${local.lambdas_dir}")
+  lambdas_source_code_dir = "../../../../lambdas"
 }
