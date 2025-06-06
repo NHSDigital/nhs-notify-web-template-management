efaf5e7f48a7bfca8cf986743fc917eb48b32200
Michael Harrison <michael.harrison31@nhs.net>
2025-06-06 13:39:19 +0100

    CCM-7939: log group in dedicated file

--- infrastructure/terraform/modules/backend-api/pipes_pipe_template_table_events.tf
+++ infrastructure/terraform/modules/backend-api/pipes_pipe_template_table_events.tf
@@ -38,12 +38,6 @@ resource "aws_pipes_pipe" "template_table_events" {
   }
 }
 
-resource "aws_cloudwatch_log_group" "pipe_template_table_events" {
-  name              = "/aws/vendedlogs/pipes/${local.csi}-template-table-events"
-  kms_key_id        = var.kms_key_arn
-  retention_in_days = var.log_retention_in_days
-}
-
 resource "aws_iam_role" "pipe_template_table_events" {
   name               = "${local.csi}-pipe-template-table-events"
   description        = "IAM Role for Pipe forward template table stream events to SQS"
