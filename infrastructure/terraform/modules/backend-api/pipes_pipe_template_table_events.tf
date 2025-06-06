e1dbfb247826ab2066c8afda989090e024923f74
Michael Harrison <michael.harrison31@nhs.net>
2025-06-06 11:57:21 +0100

    CCM-7939: handle partial batch failure

--- infrastructure/terraform/modules/backend-api/pipes_pipe_template_table_events.tf
+++ infrastructure/terraform/modules/backend-api/pipes_pipe_template_table_events.tf
@@ -9,9 +9,11 @@ resource "aws_pipes_pipe" "template_table_events" {
   source_parameters {
     dynamodb_stream_parameters {
       starting_position                  = "TRIM_HORIZON"
-      maximum_retry_attempts             = 3
-      maximum_record_age_in_seconds      = -1
+      on_partial_batch_item_failure      = "AUTOMATIC_BISECT"
+      batch_size                         = 10
       maximum_batching_window_in_seconds = 5
+      maximum_retry_attempts             = 5
+      maximum_record_age_in_seconds      = -1
 
       dead_letter_config {
         arn = module.sqs_template_table_events_pipe_dlq.sqs_queue_arn
