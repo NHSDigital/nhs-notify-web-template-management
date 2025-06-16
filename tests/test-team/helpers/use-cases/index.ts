5dd66ee6c662d542f54b726cf197733f4cb6bf0d
ben.hansell1 <ben.hansell1@nhs.net>
2025-06-16 09:06:32 +0100

    fix typecheck

--- tests/test-team/helpers/use-cases/index.ts
+++ tests/test-team/helpers/use-cases/index.ts
@@ -1,4 +1,3 @@
 export * from './simulate-failed-virus-scan';
 export * from './simulate-passed-validation';
-export * from './simulate-guard-duty-scan';
 export { UseCaseOrchestrator } from './use-case-orchestrator';
