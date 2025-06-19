0ad5b9b0c113769258f2135df86e1720e57eecb5
Michael Harrison <michael.harrison31@nhs.net>
2025-06-19 11:06:31 +0100

    CCM-10296: delete deprecated dns record. configure behavior_on_mx_failure

--- infrastructure/terraform/components/app/ses_domain_mail_from_main.tf
+++ infrastructure/terraform/components/app/ses_domain_mail_from_main.tf
@@ -1,4 +1,5 @@
 resource "aws_ses_domain_mail_from" "main" {
-  domain           = aws_ses_domain_identity.main.domain
-  mail_from_domain = "mail.${aws_ses_domain_identity.main.domain}"
+  domain                 = aws_ses_domain_identity.main.domain
+  mail_from_domain       = "mail.${aws_ses_domain_identity.main.domain}"
+  behavior_on_mx_failure = "RejectMessage"
 }
