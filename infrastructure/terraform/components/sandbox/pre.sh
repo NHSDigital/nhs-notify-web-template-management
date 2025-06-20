9fb2bbec91f9ccadd5194066407a240bb9875873
Mark Ramsden <mark.ramsden5@nhs.net>
2025-06-20 09:50:46 +0100

    CCM-10294: standardise cicd tests

--- infrastructure/terraform/components/sandbox/pre.sh
+++ infrastructure/terraform/components/sandbox/pre.sh
@@ -3,3 +3,5 @@ if [ -z "$SKIP_SANDBOX_INSTALL" ]; then npm ci; fi
 npm run generate-dependencies --workspaces --if-present
 
 npm run lambda-build --workspaces --if-present
+
+$(git rev-parse --show-toplevel)/lambdas/layers/pdfjs/build.sh
