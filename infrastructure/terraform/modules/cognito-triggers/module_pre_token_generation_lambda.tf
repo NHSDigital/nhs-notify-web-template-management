781db12ad3bd94d3540b45ff16e7285f0697b9a5
Alex Nuttall <alex.nuttall1@nhs.net>
2025-06-25 15:28:42 +0100

    rm unnecessary tf comments

--- infrastructure/terraform/modules/cognito-triggers/module_pre_token_generation_lambda.tf
+++ infrastructure/terraform/modules/cognito-triggers/module_pre_token_generation_lambda.tf
@@ -10,23 +10,19 @@ module "pre_token_generation_lambda" {
   function_name = "pre-token-generation"
   description   = "Pre token generation handler for Cognito user pool"
 
-  # function code
   function_s3_bucket      = var.function_s3_bucket
   function_code_base_path = local.lambdas_dir
   function_code_dir       = "cognito-triggers/dist"
   function_module_name    = "pre-token-generation"
   handler_function_name   = "handler"
 
-  // function config
   memory  = 512
   timeout = 3
   runtime = "nodejs20.x"
 
-  # logs
   kms_key_arn           = var.kms_key_arn
   log_retention_in_days = var.log_retention_in_days
 
-  # permissions
   permission_statements = [{
     statement_id   = "AllowCognitoInvoke"
     principal      = "cognito-idp.amazonaws.com"
