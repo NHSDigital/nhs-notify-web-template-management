69fbcf583685674fb1bb4e5825fbca969ca9cdf4
Michael Harrison <michael.harrison31@nhs.net>
2025-06-30 16:16:12 +0100

    CCM-10483: add user created provider spec file

--- tests/contracts/tests/auth/provider/user-created.provider.pact.test.ts
+++ tests/contracts/tests/auth/provider/user-created.provider.pact.test.ts
@@ -3,35 +3,35 @@ import path from 'node:path';
 import { MessageProviderPact } from '@pact-foundation/pact';
 
 // This would be the actual function that produces the event payload in the producer source code
-function createTemplateDeletedEvent() {
+function createUserCreatedEvent() {
   return {
-    'detail-type': 'TemplateDeleted',
-    source: 'uk.nhs.notify.templates',
+    'detail-type': 'UserCreated',
+    source: 'uk.nhs.notify.auth',
     time: new Date().toISOString(),
     version: '1.0',
     detail: {
-      owner: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-      id: 'de305d54-75b4-431b-adb2-eb6b9e546014',
+      clientId: '3aef04e4-4491-42ae-b24a-95c66b80cbc9',
+      userId: 'f58efb29-27a0-4da3-a502-ddd387771c1e',
     },
   };
 }
 
-describe('Pact Message Provider - TemplateDeleted Event', () => {
+describe('Pact Message Provider - UserCreated Event', () => {
   const pactDir = path.resolve(__dirname, 'pacts');
 
   const messagePact = new MessageProviderPact({
-    provider: 'templates',
+    provider: 'auth',
     pactUrls: fs
       .readdirSync(pactDir)
       .filter((f) => f.endsWith('.json'))
       .map((f) => path.join(pactDir, f)),
     messageProviders: {
-      TemplateDeleted: () => createTemplateDeletedEvent(),
+      UserCreated: () => createUserCreatedEvent(),
     },
     logLevel: 'error',
   });
 
-  it('should produce a message that satisfies the consumer contracts for TemplateDeleted', () => {
+  it('should produce a message that satisfies the consumer contracts for UserCreated', () => {
     return messagePact.verify();
   });
 });
