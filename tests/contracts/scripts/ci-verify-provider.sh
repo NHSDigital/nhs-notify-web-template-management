b9be576a3635852c35e18461bed0bf2469aa097f
Michael Harrison <michael.harrison31@nhs.net>
2025-06-30 16:36:30 +0100

    CCM-10483: iterate over providers

--- tests/contracts/scripts/ci-verify-provider.sh
+++ tests/contracts/scripts/ci-verify-provider.sh
@@ -7,29 +7,34 @@ source "${script_path}/lib/download-consumer-pacts.sh"
 
 contract_tests_root_dir=$(realpath "${script_path}/..")
 
-SUMMARY_FILE="${GITHUB_STEP_SUMMARY:-/dev/null}"
+SUMMARY_FILE="${GITHUB_STEP_SUMMARY:-"$HOME/Desktop/summary.md"}"
+# SUMMARY_FILE="${GITHUB_STEP_SUMMARY:-/dev/null}"
 
-echo "### Pact Provider Contract Test Results" >> "$SUMMARY_FILE"
+echo "### Pact Provider Contract Test Results" > "$SUMMARY_FILE"
+echo "" > "$SUMMARY_FILE"
 echo "| Provider | Consumer Pacts found? | Result |" >> "$SUMMARY_FILE"
 echo "|----------|-----------------------|--------|" >> "$SUMMARY_FILE"
 
-provider="templates"
+providers=("auth" "templates")
 
-echo "Downloading Pact files for provider: ${provider}"
+for provider in "${providers[@]}"; do
+  echo "Downloading Pact files for provider: ${provider}"
 
-count=$(download_consumer_pacts $provider)
+  count=$(download_consumer_pacts $provider)
 
-echo "Downloaded ${count} Pact files for provider: ${provider}"
+  echo "Downloaded ${count} Pact files for provider: ${provider}"
 
-if [[ "$count" -gt 0 ]]; then
-  echo "Pact contracts found — running provider contract tests..."
+  if [[ "$count" -gt 0 ]]; then
+    echo "Pact contracts found — running $provider provider contract tests..."
 
-  if npm run test:provider; then
-    echo "| ${provider} | 🟢 ${count} contracts found | 🟢 Passed |" >> "$SUMMARY_FILE"
+    if npx jest tests/$provider/provider; then
+      echo "| ${provider} | 🟢 ${count} contracts found | 🟢 Passed |" >> "$SUMMARY_FILE"
+    else
+      echo "| ${provider} | 🟢 ${count} contracts found | 🔴 Failed |" >> "$SUMMARY_FILE"
+      exit 1
+    fi
   else
-    echo "| ${provider} | 🟢 ${count} contracts found | 🔴 Failed |" >> "$SUMMARY_FILE"
-    exit 1
+    echo "No Pact contracts found from consumers — skipping $provider provider contract tests..."
+    echo "| ${provider} | 🟡 0 contracts found | 🟡 Skipped |" >> "$SUMMARY_FILE"
   fi
-else
-  echo "| ${provider} | 🟡 0 contracts found | 🟡 Skipped |" >> "$SUMMARY_FILE"
-fi
+done
--- tests/contracts/scripts/download-consumer-pacts.sh
+++ tests/contracts/scripts/download-consumer-pacts.sh
@@ -6,10 +6,12 @@ script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 source "${script_path}/lib/download-consumer-pacts.sh"
 
 # Downloads all consumer-generated Pact contract files for a provider, for use in producer-side tests
-provider="templates"
+providers=("auth" "templates")
 
-echo "Downloading Pact files for provider: ${provider}"
+for provider in "${providers[@]}"; do
+  echo "Downloading Pact files for provider: ${provider}"
 
-count=$(download_consumer_pacts $provider)
+  count=$(download_consumer_pacts $provider)
 
-echo "Downloaded ${count} Pact files for provider: ${provider}"
+  echo "Downloaded ${count} Pact files for provider: ${provider}"
+done
