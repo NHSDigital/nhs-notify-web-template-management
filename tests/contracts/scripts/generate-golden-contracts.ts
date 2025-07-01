a1c43464eec3eb1e826a8e5ea9a4b735e65701af
Michael Harrison <michael.harrison31@nhs.net>
2025-07-01 13:15:10 +0100

    CCM-10483: lint

--- tests/contracts/scripts/generate-golden-contracts.ts
+++ tests/contracts/scripts/generate-golden-contracts.ts
@@ -1,12 +1,12 @@
 import fs from 'node:fs';
 import path from 'node:path';
-import $RefParser from "@apidevtools/json-schema-ref-parser"; // eslint-disable-line import/no-extraneous-dependencies
+import $RefParser from '@apidevtools/json-schema-ref-parser'; // eslint-disable-line import/no-extraneous-dependencies
 import { zodToJsonSchema } from 'zod-to-json-schema'; // eslint-disable-line import/no-extraneous-dependencies
 import { $TemplateDeletedEvent } from '../src/templates/events/template-deleted.event';
 import { $UserCreatedEvent } from '../src/auth/events/user-created.event';
 
+// eslint-disable-next-line unicorn/prefer-top-level-await
 (async function main() {
-
   // Create template service golden contracts
   const templateSchemasPath = path.resolve(
     __dirname,
@@ -16,7 +16,7 @@ import { $UserCreatedEvent } from '../src/auth/events/user-created.event';
 
   const TemplateDeletedSchema = zodToJsonSchema(
     $TemplateDeletedEvent,
-    'TemplateDeletedEvent',
+    'TemplateDeletedEvent'
   );
   const templateDeletedPath = path.join(
     templateSchemasPath,
@@ -29,15 +29,21 @@ import { $UserCreatedEvent } from '../src/auth/events/user-created.event';
   console.log('Created JSONSchema file at', templateDeletedPath);
 
   // Create auth service golden contracts
-  const authSchemasPath = path.resolve(__dirname, '../src/auth/events/.schemas');
+  const authSchemasPath = path.resolve(
+    __dirname,
+    '../src/auth/events/.schemas'
+  );
   fs.mkdirSync(authSchemasPath, { recursive: true });
 
   const UserCreatedSchema = zodToJsonSchema(
     $UserCreatedEvent,
-    'UserCreatedEvent',
+    'UserCreatedEvent'
   );
 
   const userCreatedPath = path.join(authSchemasPath, 'UserCreated.schema.json');
-  fs.writeFileSync(userCreatedPath, JSON.stringify(await $RefParser.dereference(UserCreatedSchema), null, 2));
+  fs.writeFileSync(
+    userCreatedPath,
+    JSON.stringify(await $RefParser.dereference(UserCreatedSchema), null, 2)
+  );
   console.log('Created JSONSchema file at', userCreatedPath);
 })();
