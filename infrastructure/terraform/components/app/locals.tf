97b42631fc33f7523123dc25bae8770b31860c0f
sidnhs <siddharth.nair1@nhs.net>
2025-07-14 14:42:03 +0100

    CCM-11007: Lambda log destination fix

--- infrastructure/terraform/components/app/locals.tf
+++ infrastructure/terraform/components/app/locals.tf
@@ -2,4 +2,5 @@ locals {
   cloudfront_files_domain_name = "files.${local.root_domain_name}"
   root_domain_name             = "${var.environment}.${local.acct.dns_zone["name"]}"
   lambdas_source_code_dir      = "../../../../lambdas"
+  log_destination_arn          = var.group == "nhs-notify-template-management-prod" ? "arn:aws:logs:${var.region}:${var.observability_account_id}:destination:nhs-notify-main-acct-firehose-logs" : "arn:aws:logs:${var.region}:${var.observability_account_id}:destination:nhs-main-obs-firehose-logs" #Please remove this after we do dev/nonprod cutover for this BC
 }
