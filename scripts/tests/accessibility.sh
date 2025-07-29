f46b336fad57e9d494e803026a0fa6c609f875d5
Mark Ramsden <mark.ramsden5@nhs.net>
2025-07-29 08:59:41 +0100

    CCM-10980: debugging

--- scripts/tests/accessibility.sh
+++ scripts/tests/accessibility.sh
@@ -4,28 +4,21 @@ set -euo pipefail
 
 # feature flag
 export NEXT_PUBLIC_ENABLE_PROOFING=true
-
 export INCLUDE_AUTH_PAGES=true
 
 npm run accessibility-test-setup -w tests/accessibility
-
 npm run build
-
 mkdir -p ./frontend/public/testing
 cp ./utils/utils/src/email-templates/* ./frontend/public/testing/
 
 npm run app:start --prefix frontend
-
 npm run app:wait --prefix frontend
 
-set +e
-npm run test:accessibility -w tests/accessibility
-TEST_EXIT_CODE=$?
-set -e
+TEST_EXIT_CODE=0
+npm run test:accessibility -w tests/accessibility || TEST_EXIT_CODE=$?
 echo "TEST_EXIT_CODE=$TEST_EXIT_CODE"
 
-cp -r ./tests/accessibility/.reports/accessibility ./tests/acceptance-test-report
-
+cp -r ./tests/accessibility/.reports/accessibility ./tests/acceptance-test-report/accessibility
 npm run accessibility-test-teardown  -w tests/accessibility
 
 exit $TEST_EXIT_CODE
--- scripts/tests/ui-e2e.sh
+++ scripts/tests/ui-e2e.sh
@@ -4,14 +4,11 @@ set -euo pipefail
 cd "$(git rev-parse --show-toplevel)"
 npx playwright install --with-deps > /dev/null
 cd tests/test-team
-
-set +e
-npm run test:e2e
-TEST_EXIT_CODE=$?
-set -e
-
+TEST_EXIT_CODE=0
+npm run test:e2e || TEST_EXIT_CODE=$?
 echo "TEST_EXIT_CODE=$TEST_EXIT_CODE"
 
-cp -r ./playwright-report ../acceptance-test-report
+cp -r ./playwright-report ../acceptance-test-report/playwright-report
+[[ -e test-results ]] && cp -r ./test-results ../acceptance-test-report/test-results
 
 exit $TEST_EXIT_CODE
