09b3f602f496bd37e58a72867c4c47049654281e
Nicki Derrick <nicki.derrick1@nhs.net>
2025-07-29 17:11:31 +0100

    CCM-11390 Lint fixes

--- frontend/src/content/content.ts
+++ frontend/src/content/content.ts
@@ -324,7 +324,7 @@ const previewTemplateStatusFootnote: Partial<Record<TemplateStatus, string>> = {
   PENDING_UPLOAD: 'Refresh the page to update the status',
   PENDING_VALIDATION: 'Refresh the page to update the status',
   WAITING_FOR_PROOF: 'Refresh the page to update the status',
-}
+};
 
 const previewTemplateDetails = {
   rowHeadings: {
