6f7395813e5f073236d59bda4e142b1ea0ab4e97
Nicki Derrick <nicki.derrick1@nhs.net>
2025-08-14 13:31:26 +0100

    CCM-10893 Add unit test

--- frontend/src/__tests__/utils/truncate.test.ts
+++ frontend/src/__tests__/utils/truncate.test.ts
@@ -8,10 +8,14 @@ test('returns full string if under max length', () => {
   expect(truncate('Hello', 10)).toBe('Hello');
 });
 
+test('returns original text when exactly max length', () => {
+  expect(truncate('Hello', 5)).toBe('Hello');
+});
+
 test('truncates and adds ellipsis if over max length', () => {
   expect(truncate('This is a long string', 10)).toBe('This is a…');
 });
 
-it('trims trailing whitespace before ellipsis', () => {
+test('trims trailing whitespace before ellipsis', () => {
   expect(truncate('Hello world', 6)).toBe('Hello…');
 });
