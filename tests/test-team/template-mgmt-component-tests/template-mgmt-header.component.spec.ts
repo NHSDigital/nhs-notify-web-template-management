f5ade086dec736486d48b620460cb4460e8892fe
Nicki Derrick <nicki.derrick1@nhs.net>
2025-08-19 15:44:37 +0100

    CCM-10893 Disable eslint rule

--- tests/test-team/template-mgmt-component-tests/template-mgmt-header.component.spec.ts
+++ tests/test-team/template-mgmt-component-tests/template-mgmt-header.component.spec.ts
@@ -126,6 +126,7 @@ test.describe('Header component', () => {
   });
 
   // TODO: CCM-11148 Enable test once routing flag is implemented
+  // eslint-disable-next-line playwright/no-skipped-test
   test.skip('when user belongs to client with routing enabled, header shows both Templates and Message plans links', async ({
     page,
   }) => {
