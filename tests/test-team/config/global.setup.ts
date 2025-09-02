f945a54bf47e6ada2b90557fa0e6074dbc35ac58
ben.hansell1 <ben.hansell1@nhs.net>
2025-09-02 15:45:52 +0100

    CCM-11724: log runId

--- tests/test-team/config/global.setup.ts
+++ tests/test-team/config/global.setup.ts
@@ -2,6 +2,7 @@ import { randomUUID } from 'node:crypto';
 
 async function globalSetup() {
   process.env.PLAYWRIGHT_RUN_ID = randomUUID();
+  console.log(`=== RunId: ${process.env.PLAYWRIGHT_RUN_ID} ===`);
 }
 
 export default globalSetup;
