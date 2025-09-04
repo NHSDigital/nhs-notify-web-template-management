893eaca3b8611c3d78a811911aa6e94e3482f04b
sidnhs <siddharth.nair1@nhs.net>
2025-09-04 14:12:42 +0100

    CCM-11751: Use github release assests for shared modules call

--- infrastructure/terraform/components/sandbox/ses_receipt_rule.tf
+++ infrastructure/terraform/components/sandbox/ses_receipt_rule.tf
@@ -2,10 +2,10 @@ resource "aws_ses_receipt_rule" "proof_requested" {
   name          = "${local.csi}-store-email-proof-requested"
   rule_set_name = local.acct["ses_testing_config"].rule_set_name
 
-  recipients    = [local.sandbox_letter_supplier_mock_recipient]
-  enabled       = true
-  scan_enabled  = true
-  tls_policy    = "Optional"
+  recipients   = [local.sandbox_letter_supplier_mock_recipient]
+  enabled      = true
+  scan_enabled = true
+  tls_policy   = "Optional"
 
   s3_action {
     position          = 1
