bb72a22cd8d20e2c3bd727af5d8f425bf6e2e9df
Alex Nuttall <alex.nuttall1@nhs.net>
2025-09-09 09:57:44 +0100

    revert app eventpub]

--- infrastructure/terraform/components/app/module_eventpub.tf
+++ infrastructure/terraform/components/app/module_eventpub.tf
@@ -1,5 +1,5 @@
 module "eventpub" {
-  source = "https://github.com/NHSDigital/nhs-notify-shared-modules/releases/download/v2.0.20/terraform-eventpub.zip"
+  source = "git::https://github.com/NHSDigital/nhs-notify-shared-modules.git//infrastructure/modules/eventpub?ref=v2.0.16"
 
   name = "eventpub"
 
