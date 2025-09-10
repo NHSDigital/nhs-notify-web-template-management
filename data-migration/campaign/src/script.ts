ef983e57dc19062034b1af9d3facb456ee0f1df1
Alex Nuttall <alex.nuttall1@nhs.net>
2025-09-10 18:26:38 +0100

    dry run

--- data-migration/campaign/src/script.ts
+++ data-migration/campaign/src/script.ts
@@ -4,6 +4,7 @@ import yargs from 'yargs/yargs';
 import { hideBin } from 'yargs/helpers';
 import {
   DynamoDBDocumentClient,
+  GetCommand,
   UpdateCommand,
   UpdateCommandInput,
 } from '@aws-sdk/lib-dynamodb';
@@ -12,6 +13,16 @@ import { SendMessageCommand, SQSClient } from '@aws-sdk/client-sqs';
 import { $TemplateDtoSchema } from 'nhs-notify-backend-client';
 import { z } from 'zod/v4';
 import { GetCallerIdentityCommand, STSClient } from '@aws-sdk/client-sts';
+
+const $ExpectedTemplate = z.intersection(
+  $TemplateDtoSchema,
+  z.object({
+    templateType: z.literal('LETTER'),
+    clientId: z.string(),
+    personalisationParameters: z.array(z.string()),
+    createdBy: z.string(),
+  })
+);
 
 const ddb = DynamoDBDocumentClient.from(
   new DynamoDBClient({ region: 'eu-west-2' }),
@@ -33,7 +44,7 @@ export async function getAccountId(): Promise<string> {
   return accountId;
 }
 
-const { environment, file, newCampaignId, clientId, supplier } = yargs(
+const { environment, file, newCampaignId, clientId, supplier, realRun } = yargs(
   hideBin(process.argv)
 )
   .options({
@@ -57,11 +68,17 @@ const { environment, file, newCampaignId, clientId, supplier } = yargs(
       type: 'string',
       default: 'MBA',
     },
+    realRun: {
+      type: 'boolean',
+      default: false,
+    },
   })
   .parseSync();
 
 async function main() {
   const acct = await getAccountId();
+
+  const tableName = `nhs-notify-${environment}-app-api-templates`;
 
   // eslint-disable-next-line security/detect-non-literal-fs-filename
   const templateIds = fs
@@ -71,65 +88,76 @@ async function main() {
     .filter(Boolean);
 
   for (const id of templateIds) {
-    const updateInput: UpdateCommandInput = {
-      TableName: `nhs-notify-${environment}-app-api-templates`,
-      Key: {
-        id,
-        owner: `CLIENT#${clientId}`,
-      },
-      UpdateExpression:
-        'SET templateStatus = :waitingForProofProofStatus, files.proofs = :emptyObj, campaignId = :newId REMOVE sftpSendLockTime',
-      ExpressionAttributeValues: {
-        ':newId': newCampaignId,
-        ':pendingProofStatus': 'PENDING_PROOF_REQUEST',
-        ':waitingForProofProofStatus': 'WAITING_FOR_PROOF',
-        ':proofAvailableStatus': 'PROOF_AVAILABLE',
-        ':submittedStatus': 'SUBMITTED',
-        ':emptyObj': {},
-      },
-      ConditionExpression:
-        'attribute_exists(id) AND templateStatus in (:pendingProofStatus, :waitingForProofProofStatus, :submittedStatus, :proofAvailableStatus)',
-      ReturnValues: 'ALL_NEW',
-      ReturnValuesOnConditionCheckFailure: 'ALL_OLD',
+    const key = {
+      id,
+      owner: `CLIENT#${clientId}`,
     };
 
-    const res = await ddb.send(new UpdateCommand(updateInput));
+    try {
+      const getResult = await ddb.send(
+        new GetCommand({ TableName: tableName, Key: key })
+      );
 
-    console.log(`updated ${id}`);
+      $ExpectedTemplate.parse(getResult.Item);
 
-    const template = z
-      .intersection(
-        $TemplateDtoSchema,
-        z.object({
-          templateType: z.literal('LETTER'),
-          clientId: z.string(),
-          personalisationParameters: z.array(z.string()),
-          createdBy: z.string(),
+      console.log(`planning to update ${id}`);
+
+      const updateInput: UpdateCommandInput = {
+        TableName: tableName,
+        Key: key,
+        UpdateExpression:
+          'SET templateStatus = :waitingForProofProofStatus, files.proofs = :emptyObj, campaignId = :newId REMOVE sftpSendLockTime',
+        ExpressionAttributeValues: {
+          ':newId': newCampaignId,
+          ':pendingProofStatus': 'PENDING_PROOF_REQUEST',
+          ':waitingForProofProofStatus': 'WAITING_FOR_PROOF',
+          ':proofAvailableStatus': 'PROOF_AVAILABLE',
+          ':submittedStatus': 'SUBMITTED',
+          ':emptyObj': {},
+        },
+        ConditionExpression:
+          'attribute_exists(id) AND templateStatus in (:pendingProofStatus, :waitingForProofProofStatus, :submittedStatus, :proofAvailableStatus)',
+        ReturnValues: 'ALL_NEW',
+        ReturnValuesOnConditionCheckFailure: 'ALL_OLD',
+      };
+
+      if (!realRun) continue;
+
+      const res = await ddb.send(new UpdateCommand(updateInput));
+
+      console.log(`updated ${id}`);
+
+      const template = $ExpectedTemplate.parse(res.Attributes);
+
+      const proofRequest = {
+        campaignId: newCampaignId,
+        language: template.language,
+        letterType: template.letterType,
+        pdfVersionId: template.files.pdfTemplate.currentVersion,
+        personalisationParameters: template.personalisationParameters,
+        supplier,
+        templateId: id,
+        templateName: template.name,
+        testDataVersionId: template.files.testDataCsv?.currentVersion,
+        user: { userId: template.createdBy, clientId: template.clientId },
+      };
+
+      await sqs.send(
+        new SendMessageCommand({
+          QueueUrl: `https://sqs.eu-west-2.amazonaws.com/${acct}/nhs-notify-${environment}-app-sftp-upload-queue`,
+          MessageBody: JSON.stringify(proofRequest),
         })
-      )
-      .parse(res.Attributes);
-
-    const proofRequest = {
-      campaignId: newCampaignId,
-      language: template.language,
-      letterType: template.letterType,
-      pdfVersionId: template.files.pdfTemplate.currentVersion,
-      personalisationParameters: template.personalisationParameters,
-      supplier,
-      templateId: id,
-      templateName: template.name,
-      testDataVersionId: template.files.testDataCsv?.currentVersion,
-      user: { userId: template.createdBy, clientId: template.clientId },
-    };
-
-    await sqs.send(
-      new SendMessageCommand({
-        QueueUrl: `https://sqs.eu-west-2.amazonaws.com/${acct}/nhs-notify-${environment}-app-sftp-upload-queue`,
-        MessageBody: JSON.stringify(proofRequest),
-      })
-    );
+      );
+    } catch (error) {
+      console.error(`failed to process ${id}`);
+      throw error;
+    }
 
     console.log(`requested proof for ${id}`);
+  }
+
+  if (!realRun) {
+    console.log('run with --realRun to perform updates');
   }
 }
 
