edb1ab95cc0c452b54938841c9241bade41e0cd8
Michael Harrison <michael.harrison31@nhs.net>
2025-09-18 09:52:40 +0100

    CCM-11029: fix github token script

--- scripts/set_github_token.sh
+++ scripts/set_github_token.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+cd "$(git rev-parse --show-toplevel)"
+
 npm config --location user set @nhsdigital:registry https://npm.pkg.github.com
 
 npm config ls -l | grep '/npm.pkg.github.com/:_authToken' -q && echo "Github token already exists" && exit 0
