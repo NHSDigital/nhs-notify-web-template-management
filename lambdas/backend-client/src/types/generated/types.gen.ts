4cfd90eb284f0f44bacea074b63d469f03cad186
alex.nuttall1 <alex.nuttall1@nhs.net>
2025-10-01 11:16:42 +0100

    fix tf lambda modgss

--- lambdas/backend-client/src/types/generated/types.gen.ts
+++ lambdas/backend-client/src/types/generated/types.gen.ts
@@ -11,6 +11,13 @@ export type BaseCreatedTemplate = BaseTemplate & {
   updatedBy?: string;
 };
 
+export type BaseLetterTemplateProperties = {
+  files?: LetterFiles;
+  language: Language;
+  letterType: LetterType;
+  templateType: 'LETTER';
+};
+
 export type BaseTemplate = {
   name: string;
 };
@@ -92,83 +99,6 @@ export type CreateUpdateRoutingConfig = {
   cascade: Array<CascadeItem>;
   cascadeGroupOverrides: Array<CascadeGroup>;
   name: string;
-};
-
-export type BaseTemplate = {
-  name: string;
-};
-
-export type CascadeGroup =
-  | CascadeGroupAccessible
-  | CascadeGroupTranslations
-  | CascadeGroupStandard;
-
-export type CascadeGroupAccessible = CascadeGroupBase & {
-  accessibleFormat: Array<LetterType>;
-  name?: 'accessible';
-};
-
-export type CascadeGroupBase = {
-  name: CascadeGroupName;
-};
-
-export type CascadeGroupName = 'accessible' | 'standard' | 'translations';
-
-export type CascadeGroupStandard = CascadeGroupBase & {
-  name?: 'standard';
-};
-
-export type CascadeGroupTranslations = CascadeGroupBase & {
-  language: Array<Language>;
-  name?: 'translations';
-};
-
-export type CascadeItem = CascadeItemWithDefault | CascadeItemWithConditional;
-
-export type CascadeItemBase = {
-  cascadeGroups: Array<CascadeGroupName>;
-  channel: Channel;
-  channelType: ChannelType;
-};
-
-export type CascadeItemWithConditional = CascadeItemBase & {
-  conditionalTemplates: Array<
-    ConditionalTemplateLanguage | ConditionalTemplateAccessible
-  >;
-};
-
-export type CascadeItemWithDefault = CascadeItemBase & {
-  defaultTemplateId: string;
-};
-
-export type Channel = 'EMAIL' | 'LETTER' | 'NHSAPP' | 'SMS';
-
-export type ChannelType = 'primary' | 'secondary';
-
-export type ClientConfiguration = {
-  campaignId?: string;
-  campaignIds?: Array<string>;
-  features: ClientFeatures;
-};
-
-export type ClientConfigurationSuccess = {
-  clientConfiguration: ClientConfiguration;
-  statusCode: number;
-};
-
-export type ClientFeatures = {
-  proofing?: boolean;
-  routing?: boolean;
-};
-
-export type ConditionalTemplateAccessible = {
-  accessibleFormat: LetterType;
-  templateId: string;
-};
-
-export type ConditionalTemplateLanguage = {
-  language: Language;
-  templateId: string;
 };
 
 export type CreateUpdateTemplate = BaseTemplate &
