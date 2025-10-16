5f58c0cabb66b98efe9c6616be7213b550cd0487
Nicki Derrick <nicki.derrick1@nhs.net>
2025-10-16 15:23:31 +0100

    CCM-11492 Restructure template to separate actions and add test ID

--- frontend/src/components/molecules/MessagePlanChannelTemplate/MessagePlanChannelTemplate.tsx
+++ frontend/src/components/molecules/MessagePlanChannelTemplate/MessagePlanChannelTemplate.tsx
@@ -29,55 +29,66 @@ export function MessagePlanChannelTemplate({
       <div className={styles['channel-template-inner']}>
         <h3 className='nhsuk-heading-s'>{channelDisplayText}</h3>
 
-        {!template && (
-          <Link
-            data-testid={`choose-template-link-${channel}`}
-            className='nhsuk-link nhsuk-link--no-visited-state'
-            href={`/message-plans/${messagePlanChooseTemplateUrl(channelToTemplateType(channel))}`}
+        {template && template.id && (
+          <p
+            className='nhsuk-u-margin-bottom-2'
+            data-testid={`template-name-${channel}`}
           >
-            {content.templateLinks.choose.text}
-            <span className='nhsuk-u-visually-hidden'>
-              {channelDisplayText}
-            </span>{' '}
-            {content.templateLinks.template}
-          </Link>
+            {template.name}
+          </p>
         )}
 
-        {template && template.id && (
-          <>
-            <p>
-              {template.name}
-              <br />
+        <ul className={styles['channel-template-actions']}>
+          {!template && (
+            <li>
               <Link
-                data-testid={`change-template-link-${channel}`}
+                data-testid={`choose-template-link-${channel}`}
                 className='nhsuk-link nhsuk-link--no-visited-state'
                 href={`/message-plans/${messagePlanChooseTemplateUrl(channelToTemplateType(channel))}`}
               >
-                {content.templateLinks.change.text}
+                {content.templateLinks.choose.text}
                 <span className='nhsuk-u-visually-hidden'>
                   {channelDisplayText}
                 </span>{' '}
                 {content.templateLinks.template}
               </Link>
-            </p>
-            <p>
-              <Link
-                data-testid={`remove-template-link-${channel}`}
-                href={content.templateLinks.remove.href}
-                className={classNames(
-                  styles['channel-template-link--remove'],
-                  'nhsuk-link'
-                )}
-              >
-                {content.templateLinks.remove.text}
-                <span className='nhsuk-u-visually-hidden'>
-                  {channelDisplayText}
-                </span>{' '}
-                {content.templateLinks.template}
-              </Link>
-            </p>
-          </>
-        )}
+            </li>
+          )}
+
+          {template && template.id && (
+            <>
+              <li>
+                <Link
+                  data-testid={`change-template-link-${channel}`}
+                  className='nhsuk-link nhsuk-link--no-visited-state'
+                  href={`/message-plans/${messagePlanChooseTemplateUrl(channelToTemplateType(channel))}`}
+                >
+                  {content.templateLinks.change.text}
+                  <span className='nhsuk-u-visually-hidden'>
+                    {channelDisplayText}
+                  </span>{' '}
+                  {content.templateLinks.template}
+                </Link>
+              </li>
+              <li>
+                <Link
+                  data-testid={`remove-template-link-${channel}`}
+                  href={content.templateLinks.remove.href}
+                  className={classNames(
+                    styles['channel-template-link--remove'],
+                    'nhsuk-link'
+                  )}
+                >
+                  {content.templateLinks.remove.text}
+                  <span className='nhsuk-u-visually-hidden'>
+                    {channelDisplayText}
+                  </span>{' '}
+                  {content.templateLinks.template}
+                </Link>
+              </li>
+            </>
+          )}
+        </ul>
       </div>
     </div>
   );
