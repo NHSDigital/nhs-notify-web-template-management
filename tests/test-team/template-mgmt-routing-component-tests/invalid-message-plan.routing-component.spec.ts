cfdc3439ec0146f613687f31ec23ba35b0160069
Nicki Derrick <nicki.derrick1@nhs.net>
2025-10-21 18:16:57 +0100

    CCM-11492 Update invalid message plan test

--- tests/test-team/template-mgmt-routing-component-tests/invalid-message-plan.routing-component.spec.ts
+++ tests/test-team/template-mgmt-routing-component-tests/invalid-message-plan.routing-component.spec.ts
@@ -2,10 +2,15 @@ import { test, expect } from '@playwright/test';
 import { RoutingInvalidMessagePlanPage } from 'pages/routing/invalid-message-plan-page';
 
 test.describe('Invalid Message Plan Page', () => {
-  test('redirects to invalid message plan page for invalid ID', async ({
-    page,
-  }) => {
-    await page.goto('/message-plans/choose-templates/invalid-id');
+  test('displays invalid message plan page', async ({ page, baseURL }) => {
     const invalidMessagePlanPage = new RoutingInvalidMessagePlanPage(page);
+
+    invalidMessagePlanPage.loadPage();
+
+    await expect(page).toHaveURL(`${baseURL}/templates/message-plans/invalid`);
+
+    await expect(invalidMessagePlanPage.pageHeading).toHaveText(
+      'Sorry, we could not find that page'
+    );
   });
 });
