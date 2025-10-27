83812fa44b4e1104c3b199b8cd410d32b933f069
alex.nuttall1 <alex.nuttall1@nhs.net>
2025-10-27 16:27:21 +0000

    pipe stdout

--- tests/test-team/config/routing-component/routing-component.config.ts
+++ tests/test-team/config/routing-component/routing-component.config.ts
@@ -51,5 +51,6 @@ export default defineConfig({
     url: 'http://localhost:3000/templates/create-and-submit-templates',
     reuseExistingServer: !process.env.CI,
     stderr: 'pipe',
+    stdout: 'pipe',
   },
 });
