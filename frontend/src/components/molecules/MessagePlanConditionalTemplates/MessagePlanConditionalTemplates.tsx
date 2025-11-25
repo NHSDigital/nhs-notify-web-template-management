eb204ce12a7f7ef454eba21cdd71906d97b7a004
Nicki Derrick <nicki.derrick1@nhs.net>
2025-11-25 16:53:27 +0000

    CCM-11494 MessagePlanConditionalTemplates

---
+++ frontend/src/components/molecules/MessagePlanConditionalTemplates/MessagePlanConditionalTemplates.tsx
@@ -0,0 +1,96 @@
+import {
+  CascadeItem,
+  ConditionalTemplateAccessible,
+  ConditionalTemplateLanguage,
+  LetterType,
+  RoutingConfig,
+  TemplateDto,
+} from 'nhs-notify-backend-client';
+import {
+  MessagePlanAccessibleFormatTemplate,
+  MessagePlanLanguageTemplate,
+} from '@molecules/MessagePlanChannelTemplate/MessagePlanChannelTemplate';
+import { MessagePlanTemplates } from '@utils/routing-utils';
+import { MessagePlanFallbackConditions } from '@molecules/MessagePlanFallbackConditions/MessagePlanFallbackConditions';
+
+import styles from './MessagePlanConditionalTemplates.module.scss';
+
+const ACCESSIBLE_FORMATS: LetterType[] = ['q4']; // Large print only
+
+export function MessagePlanConditionalLetterTemplates({
+  cascadeItem,
+  cascadeIndex,
+  routingConfigId,
+  conditionalTemplates: templates,
+}: {
+  cascadeItem: CascadeItem;
+  cascadeIndex: number;
+  routingConfigId: RoutingConfig['id'];
+  conditionalTemplates: MessagePlanTemplates;
+}) {
+  if (cascadeItem.channel !== 'LETTER') {
+    return null;
+  }
+
+  const accessibleFormats = ACCESSIBLE_FORMATS;
+
+  const languageTemplates: TemplateDto[] = (
+    cascadeItem.conditionalTemplates || []
+  )
+    .filter(
+      (template): template is ConditionalTemplateLanguage =>
+        'language' in template && !!template.templateId
+    )
+    .map(({ templateId }) => templates[templateId!])
+    .filter(Boolean);
+
+  return (
+    <ul
+      className={styles['message-plan-conditional-templates']}
+      data-testid='message-plan-conditional-templates'
+    >
+      <MessagePlanFallbackConditions
+        channel={cascadeItem.channel}
+        index={cascadeIndex}
+      />
+
+      {accessibleFormats.map((format) => (
+        <li
+          key={format}
+          className={styles['message-plan-conditional-templates__list-item']}
+        >
+          <MessagePlanAccessibleFormatTemplate
+            accessibleFormat={format}
+            template={getTemplateForAccessibleFormat(
+              format,
+              cascadeItem,
+              templates
+            )}
+            routingConfigId={routingConfigId}
+          />
+        </li>
+      ))}
+
+      <li className={styles['message-plan-conditional-templates__list-item']}>
+        <MessagePlanLanguageTemplate
+          selectedTemplates={languageTemplates}
+          routingConfigId={routingConfigId}
+        />
+      </li>
+    </ul>
+  );
+}
+
+const getTemplateForAccessibleFormat = (
+  format: LetterType,
+  cascadeItem: CascadeItem,
+  templates: MessagePlanTemplates
+): TemplateDto | undefined => {
+  const conditionalTemplate = (cascadeItem.conditionalTemplates || []).find(
+    (template): template is ConditionalTemplateAccessible =>
+      'accessibleFormat' in template && template.accessibleFormat === format
+  );
+  return conditionalTemplate?.templateId
+    ? templates[conditionalTemplate.templateId]
+    : undefined;
+};
