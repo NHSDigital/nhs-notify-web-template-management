83f40476d6860e0038badfa13a6871a7884153e4
Michael Harrison <michael.harrison31@nhs.net>
2025-12-23 14:41:39 +0000

    CCM-13638: use updated shared modules for 14 day dlq retention period (#785)

--- infrastructure/terraform/modules/backend-api/module_submit_routing_config_lambda.tf
+++ infrastructure/terraform/modules/backend-api/module_submit_routing_config_lambda.tf
@@ -1,5 +1,5 @@
 module "submit_routing_config_lambda" {
-  source = "https://github.com/NHSDigital/nhs-notify-shared-modules/releases/download/v2.0.22/terraform-lambda.zip"
+  source = "https://github.com/NHSDigital/nhs-notify-shared-modules/releases/download/v2.0.28/terraform-lambda.zip"
 
   project        = var.project
   environment    = var.environment
