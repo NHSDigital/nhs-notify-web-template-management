cad7d6ad2e027744bb9ff77dff8681b2e27cdc6c
Michael Harrison <michael.harrison31@nhs.net>
2026-01-30 16:20:13 +0000

    CCM-13489: page component remove use server

--- frontend/src/app/upload-standard-english-letter-template/page.tsx
+++ frontend/src/app/upload-standard-english-letter-template/page.tsx
@@ -1,5 +1,3 @@
-'use server';
-
 import type { Metadata } from 'next';
 import { redirect, RedirectType } from 'next/navigation';
 import { NHSNotifyBackLink } from '@atoms/NHSNotifyBackLink/NHSNotifyBackLink';
