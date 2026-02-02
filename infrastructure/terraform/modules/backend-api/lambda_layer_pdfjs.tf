7a1369dd4f2989c640fefec6cf390f1105a18c78
alex.nuttall1 <alex.nuttall1@nhs.net>
2026-02-02 17:42:49 +0000

    init

--- infrastructure/terraform/modules/backend-api/lambda_layer_pdfjs.tf
+++ infrastructure/terraform/modules/backend-api/lambda_layer_pdfjs.tf
@@ -3,5 +3,5 @@ resource "aws_lambda_layer_version" "lambda_layer_pdfjs" {
   description         = "pdfjs-dist dependencies for Node.js v24"
   filename            = local.pdfjs_layer_zip
   source_code_hash    = filebase64sha256(local.pdfjs_layer_lockfile)
-  compatible_runtimes = ["nodejs24.x"]
+  compatible_runtimes = ["nodejs22.x"]
 }
