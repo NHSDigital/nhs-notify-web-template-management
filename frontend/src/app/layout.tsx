b6bbf1a045d7f5c7206e0bd83c9d14efbd294ba2
alex.nuttall1 <alex.nuttall1@nhs.net>
2026-02-11 19:01:01 +0000

    rm initAll

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -76,7 +76,7 @@ export default function RootLayout({
           </AuthProvider>
         </CookiesProvider>
         <script src={`${lib}/nhsuk-frontend-10.3.1.min.js`} type='module' />
-        <script type='module' src={`${lib}/nhsuk-frontend-init.js`} />
+        {/* <script type='module' src={`${lib}/nhsuk-frontend-init.js`} /> */}
       </body>
     </html>
   );
