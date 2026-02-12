44cc2ebfc79f804b8ca1988b3497373746a3a837
Nicki Derrick <nicki.derrick1@nhs.net>
2026-02-12 16:15:03 +0000

    CCM-14264 Add email only routing config option

--- tests/test-team/pages/routing/letter/choose-other-language-letter-template-page.ts
+++ tests/test-team/pages/routing/letter/choose-other-language-letter-template-page.ts
@@ -1,7 +1,7 @@
 import { Locator, type Page } from '@playwright/test';
-import { TemplateMgmtChooseTemplateForMessagePlanBasePage } from 'pages/template-mgmt-choose-template-base-page';
+import { RoutingChooseTemplateForMessagePlanBasePage } from 'pages/routing/choose-template-base-page';
 
-export class RoutingChooseOtherLanguageLetterTemplatePage extends TemplateMgmtChooseTemplateForMessagePlanBasePage {
+export class RoutingChooseOtherLanguageLetterTemplatePage extends RoutingChooseTemplateForMessagePlanBasePage {
   static readonly pathTemplate =
     '/message-plans/choose-other-language-letter-template/:messagePlanId';
 
