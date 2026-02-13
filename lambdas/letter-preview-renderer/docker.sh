76db64d6f8b0dd69c504abc926cbfb8e501aced1
jamesthompson26-nhs <james.thompson26@nhs.net>
2026-02-13 16:50:08 +0000

    CCM-14149: Rename AMET Image

--- lambdas/letter-preview-renderer/docker.sh
+++ lambdas/letter-preview-renderer/docker.sh
@@ -52,9 +52,8 @@ ECR_IMAGE="${ECR_REPO_URI}:${IMAGE_TAG}"
 BASE_IMAGE_ARG="$1"
 
 # Build and tag the Docker image for the lambda.
-docker build \
+docker buildx build \
   -f docker/lambda/Dockerfile \
-  --platform=linux/amd64 \
   --build-arg BASE_IMAGE="${BASE_IMAGE}" \
   -t "${ECR_IMAGE}" \
   .
