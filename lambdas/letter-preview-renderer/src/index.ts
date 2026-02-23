22a33d9f5ab092e3381150b22fecf96321cf9629
alex.nuttall1 <alex.nuttall1@nhs.net>
2026-02-23 08:39:26 +0000

    log

--- lambdas/letter-preview-renderer/src/index.ts
+++ lambdas/letter-preview-renderer/src/index.ts
@@ -2,7 +2,7 @@
 import { Handler } from 'aws-lambda';
 
 export const handler: Handler = async (event) => {
-  console.log('Received event:', event);
+  console.log('UPDATE Received event:', event);
   return {
     statusCode: 200,
     body: 'Event logged',
