a2ad6081809a56dce9add3d50ee2707d3a1eadcf
jamesthompson26-nhs <115219893+jamesthompson26-nhs@users.noreply.github.com>
2026-02-23 14:02:15 +0000

    CCM-14149: Fix Image Reference (#846)
    Co-authored-by: alex.nuttall1 <alex.nuttall1@nhs.net>

---
+++ lambdas/letter-preview-renderer/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euo pipefail
+
+rm -rf dist
+
+npx esbuild \
+  --bundle \
+  --minify \
+  --sourcemap \
+  --target=es2020 \
+  --platform=node \
+  --loader:.node=file \
+  --entry-names=[name] \
+  --outdir=dist \
+  src/index.ts
