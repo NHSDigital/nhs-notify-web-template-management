6227db07a07e31f73bd8293203e5a9397cf9262e
alex.nuttall1 <alex.nuttall1@nhs.net>
2026-02-23 19:02:03 +0000

    restore noreferrer

--- frontend/src/components/molecules/LetterRender/LetterRenderIframe.tsx
+++ frontend/src/components/molecules/LetterRender/LetterRenderIframe.tsx
@@ -14,6 +14,7 @@ export function LetterRenderIframe({ tab, pdfUrl }: LetterRenderIframeProps) {
       src={pdfUrl}
       title={`Letter preview - ${tabDescription} examples`}
       aria-label={`PDF preview of letter template with ${tabDescription} example personalisation data`}
+      referrerPolicy='no-referrer'
     />
   );
 }
