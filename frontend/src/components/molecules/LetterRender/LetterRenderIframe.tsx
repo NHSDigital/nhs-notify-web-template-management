c6e3c4955b576a1b41b12a128b61409c301b5910
alex.nuttall1 <alex.nuttall1@nhs.net>
2026-03-11 09:35:04 +0000

    log url

--- frontend/src/components/molecules/LetterRender/LetterRenderIframe.tsx
+++ frontend/src/components/molecules/LetterRender/LetterRenderIframe.tsx
@@ -10,10 +10,13 @@ export function LetterRenderIframe({ tab, pdfUrl }: LetterRenderIframeProps) {
   const tabDescription = tab === 'shortFormRender' ? 'short' : 'long';
 
   return (
-    <iframe
-      src={pdfUrl}
-      title={`Letter preview - ${tabDescription} examples`}
-      aria-label={`PDF preview of letter template with ${tabDescription} example personalisation data`}
-    />
+    <>
+      <p>{pdfUrl}</p>
+      <iframe
+        src={pdfUrl}
+        title={`Letter preview - ${tabDescription} examples`}
+        aria-label={`PDF preview of letter template with ${tabDescription} example personalisation data`}
+      />
+    </>
   );
 }
