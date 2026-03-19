47f12bb9aa8e4c5802aa98e4603ee6f047534f54
bhansell1 <ben.hansell1@nhs.net>
2026-03-19 13:51:44 +0000

    CCM-7942: use newest package

--- infrastructure/terraform/modules/backend-api/module_sqs_proof_requests_table_events_pipe_dlq.tf
+++ infrastructure/terraform/modules/backend-api/module_sqs_proof_requests_table_events_pipe_dlq.tf
@@ -1,5 +1,5 @@
 module "sqs_proof_requests_table_events_pipe_dlq" {
-  source = "https://github.com/NHSDigital/nhs-notify-shared-modules/releases/download/v2.0.28/terraform-sqs.zip"
+  source = "https://github.com/NHSDigital/nhs-notify-shared-modules/releases/download/3.0.6/terraform-sqs.zip"
 
   aws_account_id            = var.aws_account_id
   component                 = var.component
