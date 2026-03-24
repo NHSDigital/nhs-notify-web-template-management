0c7e3bcf7477321497d8b70971bb162ff186bf3f
Chris Elliott <chris.elliott17@nhs.net>
2026-03-24 13:41:16 +0000

    CCM-14758: Review comments

--- frontend/src/components/molecules/LetterRender/LetterRenderForm.tsx
+++ frontend/src/components/molecules/LetterRender/LetterRenderForm.tsx
@@ -17,7 +17,6 @@ import { PERSONALISATION_FORMDATA_PREFIX } from '@utils/constants';
 type LetterRenderFormProps = {
   template: AuthoringLetterTemplate;
   tab: PersonalisedRenderKey;
-  hideEditActions?: boolean;
 };
 
 export function LetterRenderForm({ template, tab }: LetterRenderFormProps) {
