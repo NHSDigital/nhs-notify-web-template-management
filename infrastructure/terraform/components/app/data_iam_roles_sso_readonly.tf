0fae77a508ab2625781bed786206a9e24dffe67c
jamesthompson26-nhs <james.thompson26@nhs.net>
2026-04-10 12:02:12 +0100

    CCM-16659: Readonly Role KMS Decrypt

--- infrastructure/terraform/components/app/data_iam_roles_sso_readonly.tf
+++ infrastructure/terraform/components/app/data_iam_roles_sso_readonly.tf
@@ -1,4 +1,4 @@
 data "aws_iam_roles" "sso_readonly" {
-  name_regex  = "AWSReservedSSO_permission_set_name_${var.project}_readonly_.*"
-  path_prefix = "/aws-reserved/sso.amazonaws.com/"
+  name_regex  = "AWSReservedSSO_${var.project}-readonly_.*"
+  path_prefix = "/aws-reserved/sso.amazonaws.com/${var.region}/"
 }
